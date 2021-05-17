4151a3bc0e47dcc9203b871da27da8a674eb28fd
Krystian Pudlik <kpudlik@evojam.com>
2021-05-17 17:14:25 +0200

    Add tests for combining L1Cell with empty Cell

--- src/test/scala/org/tessellation/consensus/CellMonoidTest.scala
+++ src/test/scala/org/tessellation/consensus/CellMonoidTest.scala
@@ -1,12 +1,13 @@
 package org.tessellation.consensus
 
-import cats.Applicative
+import cats.{Applicative, MonoidK}
 import cats.effect.IO
 import org.mockito.{ArgumentMatchersSugar, IdiomaticMockito}
 import org.mockito.cats.IdiomaticMockitoCats
 import org.scalatest.BeforeAndAfter
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.should.Matchers
+import org.tessellation.consensus.L1ConsensusStep.L1ConsensusMetadata
 import org.tessellation.schema.Cell.{NullTerminal, cellMonoid}
 import org.tessellation.schema.{Cell, CellError, StackF, Ω}
 
@@ -23,7 +24,7 @@ class CellMonoidTest extends AnyFreeSpec
   with BeforeAndAfter {
 
   "empty" - {
-    val M = Cell.cellMonoid[IO, StackF]
+    implicit val M: MonoidK[Cell[IO, StackF, Ω, Either[CellError, Ω], *]] = Cell.cellMonoid[IO, StackF]
 
     "creates empty" in {
       val empty = M.empty[(Int, Int)]
@@ -36,6 +37,28 @@ class CellMonoidTest extends AnyFreeSpec
       val input = empty.data
       val output = hylo.unsafeRunSync()
       output shouldBe Right(input)
+    }
+
+    "combine is an identity function (right empty)" in {
+      val l1Cell = L1Cell(L1Edge(Set.empty[L1Transaction]))
+      val emptyCell = M.empty[(L1ConsensusMetadata, Ω)]
+      val combined = M.combineK[(L1ConsensusMetadata, Ω)](
+        l1Cell.asInstanceOf[Cell[IO, StackF, Ω, Either[CellError, Ω], (L1ConsensusMetadata, Ω)]],
+        emptyCell
+      )
+
+      combined shouldBe l1Cell
+    }
+
+    "combine is an identity function (left empty)" in {
+      val l1Cell = L1Cell(L1Edge(Set.empty[L1Transaction]))
+      val emptyCell = M.empty[(L1ConsensusMetadata, Ω)]
+      val combined = M.combineK[(L1ConsensusMetadata, Ω)](
+        emptyCell,
+        l1Cell.asInstanceOf[Cell[IO, StackF, Ω, Either[CellError, Ω], (L1ConsensusMetadata, Ω)]],
+      )
+
+      combined shouldBe l1Cell
     }
   }
 
