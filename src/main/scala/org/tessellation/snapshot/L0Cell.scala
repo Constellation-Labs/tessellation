946f7c5478315677f017058215713293ec801060
Krystian Pudlik <kpudlik@evojam.com>
2021-05-20 16:12:28 +0200

    Pass whole hyloM instead of algebra and coalgebra separately to Cell

--- src/main/scala/org/tessellation/snapshot/L0Cell.scala
+++ src/main/scala/org/tessellation/snapshot/L0Cell.scala
@@ -1,10 +1,8 @@
 package org.tessellation.snapshot
 
 import cats.effect.IO
-import org.tessellation.schema.{Cell, CellError, Ω}
+import org.tessellation.schema.{Cell, CellError, StackF, Ω}
 
-case class L0Cell(edge: L0Edge) extends Cell(edge, L0Snapshot.algebra, L0Snapshot.coalgebra) {
-
-  def run(): IO[Either[CellError, Ω]] = hyloM(edge => CreateSnapshot(edge))
-
+case class L0Cell(edge: L0Edge) extends Cell[IO, StackF, L0Edge, Either[CellError, Ω], Ω](edge, L0Snapshot.hyloM) {
+  def run(): IO[Either[CellError, Ω]] = hyloM(CreateSnapshot(_))
 }
