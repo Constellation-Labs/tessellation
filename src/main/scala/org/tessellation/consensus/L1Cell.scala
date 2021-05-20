d32b1ca214c525200ebb5771785424743e0c816d
Krystian Pudlik <kpudlik@evojam.com>
2021-05-20 17:36:08 +0200

    hyloM composition

--- src/main/scala/org/tessellation/consensus/L1Cell.scala
+++ src/main/scala/org/tessellation/consensus/L1Cell.scala
@@ -1,11 +1,32 @@
 package org.tessellation.consensus
 
 import cats.effect.IO
-import org.tessellation.consensus.L1ConsensusStep.{L1ConsensusContext, L1ConsensusMetadata}
+import io.chrisdavenport.fuuid.FUUID
+import org.tessellation.Node
+import org.tessellation.consensus.L1ConsensusStep.L1ConsensusMetadata
 import org.tessellation.schema.{Cell, CellError, StackF, Ω}
 
-case class L1Cell(edge: L1Edge) extends Cell[IO, StackF, L1Edge, Either[CellError, Ω], (L1ConsensusMetadata, Ω)](edge, L1Consensus.hyloM) {
 
-  def run(context: L1ConsensusContext, cmd: L1Edge => Ω): IO[Either[CellError, Ω]] =
-    hyloM(edge => (L1ConsensusMetadata.empty(context), cmd(edge)))
+class L1Cell(edge: L1Edge) extends Cell[IO, StackF, L1Edge, Either[CellError, Ω], (L1ConsensusMetadata, Ω)](edge, L1Consensus.hyloM) {}
+
+object L1Cell {
+  def apply(edge: L1Edge): L1Cell = new L1Cell(edge)
+}
+
+case class L1StartConsensusCell(edge: L1Edge, metadata: L1ConsensusMetadata) extends L1Cell(edge) {
+  override def run(): IO[Either[CellError, Ω]] = hylo(metadata, StartOwnRound(edge))
+}
+
+object L1StartConsensusCell {
+  def fromCell[M[_], F[_]](cell: Cell[M, F, L1Edge, Either[CellError, Ω],(L1ConsensusMetadata, Ω)])(metadata: L1ConsensusMetadata): L1StartConsensusCell =
+    L1StartConsensusCell(cell.data, metadata)
+}
+
+case class L1ParticipateInConsensusCell(edge: L1Edge, metadata: L1ConsensusMetadata, roundId: FUUID, proposalNode: Node, receivedEdge: L1Edge) extends L1Cell(edge) {
+  override def run(): IO[Either[CellError, Ω]] = hylo(metadata, ReceiveProposal(roundId, proposalNode, receivedEdge, edge))
+}
+
+object L1ParticipateInConsensusCell {
+  def fromCell[M[_], F[_]](cell: Cell[M, F, L1Edge, Either[CellError, Ω],(L1ConsensusMetadata, Ω)])(metadata: L1ConsensusMetadata, roundId: FUUID, proposalNode: Node, receivedEdge: L1Edge): L1ParticipateInConsensusCell =
+    L1ParticipateInConsensusCell(cell.data, metadata, roundId, proposalNode, receivedEdge)
 }
--- src/main/scala/org/tessellation/snapshot/L0Cell.scala
+++ src/main/scala/org/tessellation/snapshot/L0Cell.scala
@@ -4,5 +4,5 @@ import cats.effect.IO
 import org.tessellation.schema.{Cell, CellError, StackF, Ω}
 
 case class L0Cell(edge: L0Edge) extends Cell[IO, StackF, L0Edge, Either[CellError, Ω], Ω](edge, L0Snapshot.hyloM) {
-  def run(): IO[Either[CellError, Ω]] = hyloM(CreateSnapshot(_))
+  override def run(): IO[Either[CellError, Ω]] = hylo(CreateSnapshot(edge))
 }
