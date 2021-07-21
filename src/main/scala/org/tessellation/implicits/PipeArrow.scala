4d4348caeeb16300cece56b5f4d2e81c26fd1504
Krystian Pudlik <kpudlik@evojam.com>
2021-07-21 21:49:10 +0200

    Include L0 into demo

---
+++ src/main/scala/org/tessellation/implicits/PipeArrow.scala
@@ -0,0 +1,20 @@
+package org.tessellation.implicits
+
+import cats.arrow.Arrow
+import cats.effect.IO
+import fs2.{Pipe, Stream}
+
+object PipeArrow {
+  implicit val arrowInstance: Arrow[Pipe[IO, *, *]] = new Arrow[Pipe[IO, *, *]] {
+    override def lift[A, B](f: A => B): Pipe[IO, A, B] = (a: Stream[IO, A]) => a.map(f)
+
+    override def compose[A, B, C](f: Pipe[IO, B, C], g: Pipe[IO, A, B]): Pipe[IO, A, C] =
+      (a: Stream[IO, A]) => f.compose(g)(a)
+
+    override def first[A, B, C](fa: Pipe[IO, A, B]): Pipe[IO, (A, C), (B, C)] =
+      (a: Stream[IO, (A, C)]) =>
+        a.flatMap {
+          case (aa, ac) => fa(Stream(aa)).map((_, ac))
+        }
+  }
+}
