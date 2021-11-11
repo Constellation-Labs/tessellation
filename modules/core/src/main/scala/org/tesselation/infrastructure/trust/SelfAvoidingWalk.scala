56e3bcb0e27228f902cf12147b221547b17ff110
ryle-goehausen <ryle.goehausen@gmail.com>
2021-11-10 22:32:48 -0800

    Enable model with simple transitive test.
    Very basic and simple data, model already has stack error fixed so enabling it should be fine. Data generator will be
    ported over separately, this is the minimal verification
    that model is producing scores and not overriding labels.

--- modules/core/src/main/scala/org/tesselation/infrastructure/trust/SelfAvoidingWalk.scala
+++ modules/core/src/main/scala/org/tesselation/infrastructure/trust/SelfAvoidingWalk.scala
@@ -337,16 +337,15 @@ object SelfAvoidingWalk extends StrictLogging {
   ): TrustNode = {
 
     var nodesCycle = nodes
-    // TODO: Fix stack overflow issue
-    //    if (nodesCycle.size > 2) { //note, need min of 3 nodes
-    //      println(s"runWalkFeedbackUpdateSingleNode nodes ${nodes.toList} for node $selfId")
-    //      (0 until feedbackCycles).foreach { cycle =>
-    //        println(s"feedback cycle $cycle for node $selfId")
-    //        nodesCycle = runWalkBatchesFeedback(selfId, nodes, batchIterationSize, epsilon, maxIterations)
-    //      }
-    //    }
+    if (nodesCycle.size > 2) { //note, need min of 3 nodes
+      //          println(s"runWalkFeedbackUpdateSingleNode nodes ${nodes.toList} for node $selfId")
+      (0 until feedbackCycles).foreach { cycle =>
+        //            println(s"feedback cycle $cycle for node $selfId")
+        nodesCycle = runWalkBatchesFeedback(selfId, nodes, batchIterationSize, epsilon, maxIterations)
+      }
+    }
     val res: TrustNode = nodesCycle.filter(_.id == selfId).head
-    println(s"runWalkFeedbackUpdateSingleNode res: TrustNode ${res} for node $selfId")
+//    println(s"runWalkFeedbackUpdateSingleNode res: TrustNode ${res} for node $selfId")
 
     res
   }
