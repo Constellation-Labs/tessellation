6cd0d6afde4a770a1a3de792079194bfc0def08a
ryle-goehausen <ryle.goehausen@gmail.com>
2021-11-30 19:53:06 -0800

    Update generator to new cats, add basic test.
    Preparation for sybil attack example next, verify data generator
    is still working properly and generating proper edges.

---
+++ modules/core/src/test/scala/org/tessellation/trust/DataGenerator.scala
@@ -0,0 +1,65 @@
+package org.tessellation.trust
+
+import cats.effect.std.Random
+import cats.syntax.all._
+import cats.{Applicative, Monad}
+
+import org.tessellation.infrastructure.trust.{TrustEdge, TrustNode}
+
+class DataGenerator[F[_]: Monad: Random] {
+
+  def randomEdgeLogic(distance: Double): F[Boolean] =
+    for {
+      value1 <- Random[F].nextDouble
+      value2 <- Random[F].nextDouble
+    } yield {
+      value1 > distance && value2 < 0.5
+    }
+
+  def randomEdge(logic: Double => F[Boolean] = randomEdgeLogic)(n: TrustNode, n2: TrustNode) =
+    for {
+      result <- logic(n.id.toDouble)
+      trustZeroToOne <- Random[F].nextDouble
+    } yield {
+      if (result) {
+        Some(TrustEdge(n.id, n2.id, 2 * (trustZeroToOne - 0.5), isLabel = true))
+      } else None
+    }
+
+  def seedCliqueLogic(maxSeedNodeIdx: Int = 1)(id: Double): F[Boolean] =
+    Applicative[F].pure(id <= maxSeedNodeIdx)
+
+  def cliqueEdge(logic: Double => F[Boolean] = seedCliqueLogic())(n: TrustNode, n2: TrustNode): F[Option[TrustEdge]] =
+    for {
+      result <- logic(n.distance(n2))
+      trustZeroToOne <- Random[F].nextDouble
+    } yield
+      if (result) Some(TrustEdge(n.id, n2.id, 1.0, isLabel = true))
+      else {
+        Some(TrustEdge(n.id, n2.id, 2 * (trustZeroToOne - 0.5)))
+      }
+
+  def generateData(
+    numNodes: Int = 30,
+    edgeLogic: (TrustNode, TrustNode) => F[Option[TrustEdge]] = randomEdge()
+  ): F[List[TrustNode]] =
+    for {
+      x <- Random[F].nextDouble
+      y <- Random[F].nextDouble
+      nodes = (0 until numNodes).toList.map(id => TrustNode(id, x, y))
+      nodesWithEdges = nodes.traverse { n =>
+        val edges = nodes.filterNot(_.id == n.id).traverse { n2 =>
+          edgeLogic(n, n2)
+        }
+        edges.map(e => n.copy(edges = e.flatten))
+      }
+      res <- nodesWithEdges
+    } yield res
+
+  def bipartiteEdge(logic: Double => F[Boolean] = seedCliqueLogic())(n: TrustNode, n2: TrustNode) =
+    for {
+      result <- logic(n.id.toDouble)
+    } yield
+      if (result) Some(TrustEdge(n.id, n2.id, 1.0, isLabel = true))
+      else Some(TrustEdge(n.id, n2.id, -1.0))
+}
