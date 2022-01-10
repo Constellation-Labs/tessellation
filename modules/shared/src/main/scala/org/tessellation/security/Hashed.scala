13269e5d5f3cef513c2702124bca6f3e7bac65a2
Maciej Muszyński <30803273+TheMMaciek@users.noreply.github.com>
2022-01-10 17:45:16 +0100

    Transaction and Hashed orderings

--- modules/shared/src/main/scala/org/tessellation/security/Hashed.scala
+++ modules/shared/src/main/scala/org/tessellation/security/Hashed.scala
@@ -1,5 +1,7 @@
 package org.tessellation.security
 
+import cats.Order
+
 import org.tessellation.security.hash.{Hash, ProofsHash}
 import org.tessellation.security.signature.Signed
 
@@ -8,3 +10,11 @@ import derevo.derive
 
 @derive(eqv, show)
 case class Hashed[A <: AnyRef](signed: Signed[A], hash: Hash, proofsHash: ProofsHash)
+
+object Hashed {
+  implicit def autoUnwrap[T <: AnyRef](t: Hashed[T]): T = t.signed
+
+  implicit def order[A <: AnyRef: Order]: Order[Hashed[A]] = (x: Hashed[A], y: Hashed[A]) => Order[A].compare(x, y)
+
+  implicit def ordering[A <: AnyRef: Order]: Ordering[Hashed[A]] = order.toOrdering
+}
