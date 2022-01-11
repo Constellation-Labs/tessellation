21d69bd7934c389ecefbbc2e7d9c00cdafadb49d
Maciej Muszyński <30803273+TheMMaciek@users.noreply.github.com>
2022-01-11 16:00:07 +0100

    Refactor transaction and block services to become storages

--- modules/dag-l1/src/main/scala/org/tessellation/dag/l1/domain/consensus/block/BlockConsensusContext.scala
+++ modules/dag-l1/src/main/scala/org/tessellation/dag/l1/domain/consensus/block/BlockConsensusContext.scala
@@ -3,22 +3,22 @@ package org.tessellation.dag.l1.domain.consensus.block
 import java.security.KeyPair
 
 import org.tessellation.dag.block.BlockValidator
-import org.tessellation.dag.l1.domain.block.BlockService
+import org.tessellation.dag.l1.domain.block.BlockStorage
 import org.tessellation.dag.l1.domain.consensus.block.config.ConsensusConfig
 import org.tessellation.dag.l1.domain.consensus.block.http.p2p.clients.BlockConsensusClient
 import org.tessellation.dag.l1.domain.consensus.block.storage.ConsensusStorage
-import org.tessellation.dag.l1.domain.transaction.TransactionService
+import org.tessellation.dag.l1.domain.transaction.TransactionStorage
 import org.tessellation.schema.peer.PeerId
 import org.tessellation.sdk.domain.cluster.storage.ClusterStorage
 
 case class BlockConsensusContext[F[_]](
   blockConsensusClient: BlockConsensusClient[F],
-  blockService: BlockService[F],
+  blockStorage: BlockStorage[F],
   blockValidator: BlockValidator[F],
   clusterStorage: ClusterStorage[F],
   consensusConfig: ConsensusConfig,
   consensusStorage: ConsensusStorage[F],
   keyPair: KeyPair,
   selfId: PeerId,
-  transactionService: TransactionService[F]
+  transactionStorage: TransactionStorage[F]
 )
