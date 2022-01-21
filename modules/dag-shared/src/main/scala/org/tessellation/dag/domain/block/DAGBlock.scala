035f8fe97e0c49e34bef0dcf2842039b6b2ba0f7
Marcin Wadoń <mwadon@evojam.com>
2022-01-21 16:04:04 +0100

    Fiber definition for DAG Transaction and Block

--- modules/dag-shared/src/main/scala/org/tessellation/dag/domain/block/DAGBlock.scala
+++ modules/dag-shared/src/main/scala/org/tessellation/dag/domain/block/DAGBlock.scala
@@ -4,6 +4,7 @@ import cats.Order
 import cats.data.NonEmptyList
 import cats.syntax.reducible._
 
+import org.tessellation.schema.Fiber
 import org.tessellation.schema.height.Height
 import org.tessellation.schema.transaction.Transaction
 import org.tessellation.security.signature.Signed
@@ -12,9 +13,16 @@ import derevo.circe.magnolia.{decoder, encoder}
 import derevo.derive
 import io.estatico.newtype.ops._
 
+case class DAGBlockReference(parents: NonEmptyList[BlockReference])
+case class DAGBlockData(transactions: Set[Signed[Transaction]])
+
 @derive(encoder, decoder)
-case class DAGBlock(transactions: Set[Signed[Transaction]], parent: NonEmptyList[BlockReference]) {
+case class DAGBlock(transactions: Set[Signed[Transaction]], parent: NonEmptyList[BlockReference])
+    extends Fiber[DAGBlockReference, DAGBlockData] {
   val height: Height = Height(parent.minimum.height.coerce + 1L)
+
+  def reference = DAGBlockReference(parent)
+  def data = DAGBlockData(transactions)
 }
 
 object DAGBlock {
--- modules/shared/src/main/scala/org/tessellation/schema/Fiber.scala
+++ modules/shared/src/main/scala/org/tessellation/schema/Fiber.scala
@@ -1,6 +1,6 @@
 package org.tessellation.schema
 
 trait Fiber[A, B] {
-  def parent: A
-  def reference: B
+  def reference: A
+  def data: B
 }
