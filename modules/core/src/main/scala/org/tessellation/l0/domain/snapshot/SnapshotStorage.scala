5d88488ae6978bdeaf3bc5187ace5ddd8781b2a7
Krystian Pudlik <kpudlik@evojam.com>
2022-01-25 02:00:05 +0100

    Fallback to time-based trigger

--- modules/core/src/main/scala/org/tessellation/l0/domain/snapshot/SnapshotStorage.scala
+++ modules/core/src/main/scala/org/tessellation/l0/domain/snapshot/SnapshotStorage.scala
@@ -1,7 +1,13 @@
 package org.tessellation.l0.domain.snapshot
+
+import org.tessellation.kernel.StateChannelSnapshot
+import org.tessellation.l0.schema.snapshot.{GlobalSnapshot, GlobalSnapshotOrdinal}
 import org.tessellation.schema.height.Height
 
 trait SnapshotStorage[F[_]] {
   def storeSnapshot(snapshot: GlobalSnapshot): F[Unit]
   def getLastSnapshotHeight: F[Height]
+  def getLastSnapshotSubHeight: F[Height]
+  def getLastSnapshotOrdinal: F[GlobalSnapshotOrdinal]
+  def getStateChannelSnapshots: F[Set[StateChannelSnapshot]]
 }
