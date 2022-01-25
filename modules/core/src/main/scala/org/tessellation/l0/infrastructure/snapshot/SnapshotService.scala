c3b15f257b78a24be041d6e6582956ccf697e0e8
Krystian Pudlik <kpudlik@evojam.com>
2022-01-25 01:59:57 +0100

    Base types

--- modules/core/src/main/scala/org/tessellation/l0/infrastructure/snapshot/SnapshotService.scala
+++ modules/core/src/main/scala/org/tessellation/l0/infrastructure/snapshot/SnapshotService.scala
@@ -5,17 +5,16 @@ import cats.effect.kernel.Async
 import cats.syntax.functor._
 
 import scala.util.control.NoStackTrace
-
 import org.tessellation.dag.domain.block.DAGBlock
 import org.tessellation.kernel.StateChannelSnapshot
-import org.tessellation.l0.domain.snapshot.{GlobalSnapshot, SnapshotService, SnapshotStorage}
+import org.tessellation.l0.domain.snapshot.{GlobalSnapshot, GlobalSnapshotOrdinal, SnapshotService, SnapshotStorage}
 import org.tessellation.schema.height.Height
 import org.tessellation.schema.peer.PeerId
 import org.tessellation.security.Hashed
-
 import eu.timepit.refined.auto._
-import eu.timepit.refined.types.numeric.PosLong
+import eu.timepit.refined.types.numeric.{NonNegLong, PosLong}
 import io.estatico.newtype.ops._
+import org.tessellation.security.signature.Signed
 
 object SnapshotService {
 
@@ -26,8 +25,22 @@ object SnapshotService {
   ): SnapshotService[F] =
     new SnapshotService[F] {
 
+      def createEmptySnapshot(
+        prev: GlobalSnapshot,
+        snapshots: Set[StateChannelSnapshot],
+        nextFacilitators: NonEmptySet[PeerId]
+      ): GlobalSnapshot =
+        GlobalSnapshot(
+          ordinal = GlobalSnapshotOrdinal(prev.ordinal.coerce + NonNegLong(1)),
+          height = prev.height,
+          subHeight = Height(prev.subHeight.coerce + NonNegLong(1)),
+          blocks = Set.empty[Signed[DAGBlock]],
+          snapshots = snapshots,
+          nextFacilitators = nextFacilitators
+        )
+
       def createSnapshot(
-        blocks: NonEmptySet[Hashed[DAGBlock]],
+        blocks: Set[Hashed[DAGBlock]],
         snapshots: Set[StateChannelSnapshot],
         nextFacilitators: NonEmptySet[PeerId]
       ): F[GlobalSnapshot] =
