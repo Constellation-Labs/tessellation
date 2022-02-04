92874c7bb74670f8e7058bb28f3a2ee80bb2d327
Marcin Wadoń <mwadon@evojam.com>
2022-02-04 11:08:16 +0100

    Healthcheck - round start

--- modules/sdk/src/main/scala/org/tessellation/sdk/domain/healthcheck/HealthCheckRound.scala
+++ modules/sdk/src/main/scala/org/tessellation/sdk/domain/healthcheck/HealthCheckRound.scala
@@ -1,9 +1,13 @@
 package org.tessellation.sdk.domain.healthcheck
 
+import cats.Monad
+import cats.effect.Ref
+import cats.syntax.functor._
+
 import org.tessellation.schema.peer.{Peer, PeerId}
 import org.tessellation.sdk.domain.healthcheck.types._
 
-class HealthCheckRound[F[_], A <: HealthCheckStatus, B <: ConsensusHealthStatus[A]] {
+class HealthCheckRound[F[_], A <: HealthCheckStatus, B <: ConsensusHealthStatus[A]]() {
   def getPeers: F[Set[Peer]] = ???
   def manageAbsent(peers: Set[PeerId]) = ???
   def isFinished: F[Boolean] = ???
@@ -11,4 +15,17 @@ class HealthCheckRound[F[_], A <: HealthCheckStatus, B <: ConsensusHealthStatus[
   def generateHistoricalData(decision: HealthCheckConsensusDecision): F[HistoricalRound] = ???
   def calculateOutcome: F[HealthCheckConsensusDecision] = ???
   def processProposal(proposal: B): F[Unit] = ???
+  def start: F[Unit] = ???
+  def getRoundIds: F[Set[HealthCheckRoundId]] = ???
+  def addParallelRounds(key: HealthCheckKey)(roundIds: Set[HealthCheckRoundId]): F[Unit] = ???
+}
+
+object HealthCheckRound {
+
+  def make[F[_]: Ref.Make: Monad, A <: HealthCheckStatus, B <: ConsensusHealthStatus[A]](
+    // ...
+  ): F[HealthCheckRound[F, A, B]] =
+    Ref.of[F, Set[PeerId]](Set.empty).map { peers =>
+      new HealthCheckRound[F, A, B]()
+    }
 }
