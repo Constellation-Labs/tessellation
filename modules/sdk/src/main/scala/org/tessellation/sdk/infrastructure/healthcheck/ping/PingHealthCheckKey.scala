aa17009cd673ff17a7131cf253729f1e024e853b
Marcin Wadoń <mwadon@evojam.com>
2022-06-02 16:26:18 +0200

    Check session while removing peer from ping healthcheck

--- modules/sdk/src/main/scala/org/tessellation/sdk/infrastructure/healthcheck/ping/PingHealthCheckKey.scala
+++ modules/sdk/src/main/scala/org/tessellation/sdk/infrastructure/healthcheck/ping/PingHealthCheckKey.scala
@@ -1,8 +1,9 @@
 package org.tessellation.sdk.infrastructure.healthcheck.ping
 
+import org.tessellation.schema.cluster.SessionToken
 import org.tessellation.schema.peer.PeerId
 import org.tessellation.sdk.domain.healthcheck.consensus.types.HealthCheckKey
 
 import com.comcast.ip4s.{Host, Port}
 
-case class PingHealthCheckKey(id: PeerId, ip: Host, p2pPort: Port) extends HealthCheckKey
+case class PingHealthCheckKey(id: PeerId, ip: Host, p2pPort: Port, session: SessionToken) extends HealthCheckKey
