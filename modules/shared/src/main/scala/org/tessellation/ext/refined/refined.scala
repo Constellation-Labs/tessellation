796b2d8b4dd014a07ca77c78ec165423b6c43421
Krystian Pudlik <kpudlik@evojam.com>
2022-08-04 17:27:21 +0200

    Move NonNegLong arithmetic operations to ext package

--- modules/shared/src/main/scala/org/tessellation/ext/refined/refined.scala
+++ modules/shared/src/main/scala/org/tessellation/ext/refined/refined.scala
@@ -1,12 +1,17 @@
-package org.tessellation.infrastructure.rewards
+package org.tessellation.ext
 
-import cats.syntax.all._
-
+import _root_.cats.syntax.bifunctor._
+import _root_.cats.syntax.either._
+import _root_.cats.syntax.order._
+import _root_.cats.{Eq, Order, Show}
+import eu.timepit.refined.api.{Refined, Validate}
 import eu.timepit.refined.auto._
 import eu.timepit.refined.cats._
+import eu.timepit.refined.refineV
 import eu.timepit.refined.types.numeric.{NonNegLong, PosLong}
+import io.circe.{Decoder, Encoder}
 
-object Arithmetic {
+package object refined {
 
   implicit class NonNegLongOps(a: NonNegLong) {
 
@@ -37,5 +42,22 @@ object Arithmetic {
         new ArithmeticException("division by zero")
       )
   }
+
+  // For exemplary validator definition look into DAGAddressRefined object
+
+  def decoderOf[T, P](implicit v: Validate[T, P], d: Decoder[T]): Decoder[T Refined P] =
+    d.emap(refineV[P].apply[T](_))
+
+  def encoderOf[T, P](implicit e: Encoder[T]): Encoder[T Refined P] =
+    e.contramap(_.value)
+
+  def eqOf[T, P](implicit eqT: Eq[T]): Eq[T Refined P] =
+    Eq.instance((a, b) => eqT.eqv(a.value, b.value))
+
+  def showOf[T, P](implicit showT: Show[T]): Show[T Refined P] =
+    Show.show(r => showT.show(r.value))
+
+  def orderOf[T, P](implicit orderT: Order[T]): Order[T Refined P] =
+    (x: T Refined P, y: T Refined P) => x.value.compare(y.value)
 
 }
