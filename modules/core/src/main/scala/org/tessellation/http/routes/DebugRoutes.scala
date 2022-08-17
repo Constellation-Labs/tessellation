efe774088219248f1fed94b2a79b8b217f5dd0ac
Tomasz Bekas <tomasz.bekas@gmail.com>
2022-08-17 11:25:15 +0200

    Consensus debug endpoints

--- modules/core/src/main/scala/org/tessellation/http/routes/DebugRoutes.scala
+++ modules/core/src/main/scala/org/tessellation/http/routes/DebugRoutes.scala
@@ -1,14 +1,18 @@
 package org.tessellation.http.routes
 
 import cats.effect.Async
-import cats.syntax.applicativeError._
-import cats.syntax.flatMap._
-import cats.syntax.option._
+import cats.syntax.all._
 
+import org.tessellation.ext.http4s.SnapshotOrdinalVar
 import org.tessellation.modules.{Services, Storages}
 import org.tessellation.schema.cluster.SessionAlreadyExists
 import org.tessellation.schema.node.InvalidNodeStateTransition
+import org.tessellation.schema.peer.PeerId
+import org.tessellation.sdk.infrastructure.consensus.declaration.PeerDeclaration
+import org.tessellation.sdk.infrastructure.consensus.{ConsensusResources, PeerDeclarations}
 
+import derevo.circe.magnolia.{decoder, encoder}
+import derevo.derive
 import org.http4s.HttpRoutes
 import org.http4s.circe.CirceEntityCodec.circeEntityEncoder
 import org.http4s.dsl.Http4sDsl
@@ -33,8 +37,36 @@ final case class DebugRoutes[F[_]: Async](
       services.gossip.spread(intContent.some) >> Ok()
     case POST -> Root / "gossip" / "spread" / strContent =>
       services.gossip.spreadCommon(strContent) >> Ok()
+    case GET -> Root / "consensus" / SnapshotOrdinalVar(ordinal) / "resources" =>
+      services.consensus.storage
+        .getResources(ordinal)
+        .map(ConsensusResourcesView.fromResources)
+        .flatMap(Ok(_))
+    case GET -> Root / "consensus" / SnapshotOrdinalVar(ordinal) / "facilitators" =>
+      services.consensus.storage.getState(ordinal).map(_.map(_.facilitators)).flatMap {
+        _.map(Ok(_)).getOrElse(NotFound())
+      }
   }
 
+  @derive(encoder, decoder)
+  case class ConsensusResourcesView(
+    facility: List[PeerId],
+    proposal: List[PeerId],
+    signature: List[PeerId]
+  )
+
+  object ConsensusResourcesView {
+    def fromResources(resources: ConsensusResources[_]): ConsensusResourcesView = {
+      def peersWithDeclaration(fn: PeerDeclarations => Option[PeerDeclaration]): List[PeerId] =
+        resources.peerDeclarationsMap.toList.mapFilter { case (peerId, pds) => fn(pds).map(_ => peerId) }
+
+      ConsensusResourcesView(
+        peersWithDeclaration(_.facility),
+        peersWithDeclaration(_.proposal),
+        peersWithDeclaration(_.signature)
+      )
+    }
+  }
   val routes: HttpRoutes[F] = Router(
     prefixPath -> httpRoutes
   )
