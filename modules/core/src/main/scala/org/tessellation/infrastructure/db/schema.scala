74d8cc1b27471ef2f71375710ae90ab64b31626a
Marcin Wadoń <mwadon@evojam.com>
2022-08-23 12:31:14 +0200

    Use in-memory storage for AddressStorage

--- modules/core/src/main/scala/org/tessellation/infrastructure/db/schema.scala
+++
@@ -1,8 +0,0 @@
-package org.tessellation.infrastructure.db
-
-import org.tessellation.schema.address.Address
-import org.tessellation.schema.balance.Balance
-
-object schema {
-  case class StoredAddress(address: Address, balance: Balance)
-}
--- modules/dag-l1/src/main/resources/db/migration/V1.0.0.0__AddressTable.sql
+++
@@ -1,5 +0,0 @@
-create table if not exists Address (
-    address varchar(40) primary key,
-    balance bigint default 0
-    constraint balance_non_negative check (balance >= 0)
-);
