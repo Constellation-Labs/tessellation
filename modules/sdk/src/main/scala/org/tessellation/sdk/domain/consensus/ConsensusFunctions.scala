c74293d368c39fa4a1ff48c21db4b11bbc69e45c
Tomasz Bekas <tomasz.bekas@gmail.com>
2022-10-19 10:16:22 +0200

    Filter proposed facilitators based on collateral

--- modules/sdk/src/main/scala/org/tessellation/sdk/domain/consensus/ConsensusFunctions.scala
+++ modules/sdk/src/main/scala/org/tessellation/sdk/domain/consensus/ConsensusFunctions.scala
@@ -1,11 +1,14 @@
 package org.tessellation.sdk.domain.consensus
 
+import org.tessellation.schema.peer.PeerId
 import org.tessellation.sdk.infrastructure.consensus.trigger.ConsensusTrigger
 import org.tessellation.security.signature.Signed
 
 trait ConsensusFunctions[F[_], Event, Key, Artifact] {
 
   def triggerPredicate(event: Event): Boolean
+
+  def facilitatorFilter(lastSignedArtifact: Signed[Artifact], peerId: PeerId): F[Boolean]
 
   def createProposalArtifact(
     lastKey: Key,
