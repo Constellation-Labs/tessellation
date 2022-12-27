3cd2977a9b58bd0da57c3d294920c758eb0358e2
Tomasz Bekas <tomasz.bekas@gmail.com>
2022-12-27 15:08:21 +0100

    Validate kryo registrations

--- modules/shared/src/main/scala/org/tessellation/kryo/ExplicitKryoRegistrar.scala
+++ modules/shared/src/main/scala/org/tessellation/kryo/ExplicitKryoRegistrar.scala
@@ -1,5 +1,6 @@
 package org.tessellation.kryo
 
+import com.esotericsoftware.kryo.Registration
 import com.esotericsoftware.kryo.serializers.CompatibleFieldSerializer
 import com.twitter.chill.{IKryoRegistrar, Kryo, ObjectSerializer}
 
@@ -18,10 +19,23 @@ case class ExplicitKryoRegistrar(registrar: Map[Class[_], Int]) extends IKryoReg
           fs
         }
 
+        val resolver = k.getClassResolver
+
+        throwIfRegistrationExists(klass, id, resolver.getRegistration(id))
+        throwIfRegistrationExists(klass, id, resolver.getRegistration(klass))
+
         k.register(klass, ser, id)
         ()
     }
 
   def isScalaObject(klass: Class[_]): Boolean =
     klass.getName.last == '$' && objectSerializer.accepts(klass)
+
+  def throwIfRegistrationExists(klass: Class[_], id: Int, registrationOrNull: Registration): Unit =
+    if (registrationOrNull != null)
+      throw new Throwable(
+        s"Attempt to create registration ${klass.getName}->$id when there's an existing" +
+          s" registration ${registrationOrNull.getType.getName}->${registrationOrNull.getId}"
+      )
+
 }
