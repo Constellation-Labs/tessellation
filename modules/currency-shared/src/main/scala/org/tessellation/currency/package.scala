774157f6b880de76366d97bca5e9899ed76f73ce
Maciej Muszyński <30803273+TheMMaciek@users.noreply.github.com>
2023-03-01 14:34:02 +0100

    Extract common part for currency to shared module

---
+++ modules/currency-shared/src/main/scala/org/tessellation/currency/package.scala
@@ -0,0 +1,21 @@
+package org.tessellation
+
+import org.tessellation.currency.schema.currency._
+import org.tessellation.ext.kryo.KryoRegistrationId
+
+import eu.timepit.refined.auto._
+import eu.timepit.refined.numeric.Interval
+
+package object currency {
+
+  type CurrencyKryoRegistrationIdRange = Interval.Closed[900, 999]
+  type CurrencyKryoRegistrationId = KryoRegistrationId[CurrencyKryoRegistrationIdRange]
+
+  val currencyKryoRegistrar: Map[Class[_], CurrencyKryoRegistrationId] = Map(
+    classOf[CurrencyTransaction] -> 900,
+    classOf[CurrencyBlock] -> 901,
+    classOf[CurrencySnapshotInfo] -> 902,
+    classOf[CurrencySnapshot] -> 903,
+    CurrencyBlock.OrderingInstanceAsActiveTip.getClass -> 904
+  )
+}
