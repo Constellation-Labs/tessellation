f24ffe2164ec7e9deb440dc90784fe4db3933d82
Krystian Pudlik <krystian.pudlik@gmail.com>
2023-03-29 12:07:20 +0200

    Add version field to Incremental Snapshot

--- modules/shared/src/main/scala/org/tessellation/schema/snapshot.scala
+++ modules/shared/src/main/scala/org/tessellation/schema/snapshot.scala
@@ -11,6 +11,7 @@ import org.tessellation.kryo.KryoSerializer
 import org.tessellation.schema.address.Address
 import org.tessellation.schema.balance.Balance
 import org.tessellation.schema.height.{Height, SubHeight}
+import org.tessellation.schema.semver.SnapshotVersion
 import org.tessellation.schema.transaction.{Transaction, TransactionReference}
 import org.tessellation.security.hash.Hash
 import org.tessellation.syntax.sortedCollection._
@@ -28,6 +29,7 @@ object snapshot {
 
   trait IncrementalSnapshot[T <: Transaction, B <: Block[T], P <: StateProof] extends Snapshot[T, B] {
     val stateProof: P
+    val version: SnapshotVersion
   }
 
   trait Snapshot[T <: Transaction, B <: Block[T]] {
