064b3fca79b022c206b582ffe420f54a9591544e
Krystian Pudlik <krystian.pudlik@gmail.com>
2023-06-13 15:15:00 +0200

    Expose CurrencyL0App and CurrencyL1App

---
+++ modules/currency-l0/src/main/scala/org/tessellation/currency/l0/CurrencyL0App.scala
@@ -0,0 +1,170 @@
+package org.tessellation.currency.l0
+
+import cats.effect.{IO, Resource}
+import cats.syntax.semigroupk._
+import cats.syntax.traverse._
+
+import org.tessellation.BuildInfo
+import org.tessellation.currency._
+import org.tessellation.currency.l0.cli.method
+import org.tessellation.currency.l0.cli.method._
+import org.tessellation.currency.l0.http.P2PClient
+import org.tessellation.currency.l0.modules._
+import org.tessellation.currency.schema.currency._
+import org.tessellation.ext.cats.effect.ResourceIO
+import org.tessellation.ext.kryo._
+import org.tessellation.schema.cluster.ClusterId
+import org.tessellation.schema.node.NodeState
+import org.tessellation.sdk.app.{SDK, TessellationIOApp}
+import org.tessellation.sdk.domain.rewards.Rewards
+import org.tessellation.sdk.infrastructure.gossip.{GossipDaemon, RumorHandlers}
+import org.tessellation.sdk.resources.MkHttpServer
+import org.tessellation.sdk.resources.MkHttpServer.ServerName
+import org.tessellation.sdk.{SdkOrSharedOrKernelRegistrationIdRange, sdkKryoRegistrar}
+import org.tessellation.security.SecurityProvider
+
+import com.monovore.decline.Opts
+
+abstract class CurrencyL0App(
+  name: String,
+  header: String,
+  clusterId: ClusterId,
+  version: String
+) extends TessellationIOApp[Run](
+      name,
+      header,
+      clusterId,
+      version = version
+    ) {
+
+  val opts: Opts[Run] = method.opts
+
+  type KryoRegistrationIdRange = SdkOrSharedOrKernelRegistrationIdRange
+
+  val kryoRegistrar: Map[Class[_], KryoRegistrationId[KryoRegistrationIdRange]] =
+    sdkKryoRegistrar
+
+  def dataApplication: Option[BaseDataApplicationL0Service[IO]]
+  def rewards(
+    implicit sp: SecurityProvider[IO]
+  ): Option[Rewards[IO, CurrencyTransaction, CurrencyBlock, CurrencySnapshotStateProof, CurrencyIncrementalSnapshot]]
+
+  def run(method: Run, sdk: SDK[IO]): Resource[IO, Unit] = {
+    import sdk._
+
+    val cfg = method.appConfig
+
+    for {
+      queues <- Queues.make[IO](sdkQueues).asResource
+      p2pClient = P2PClient.make[IO](sdkP2PClient, sdkResources.client, method.identifier, sdkServices.session)
+      storages <- Storages.make[IO](sdkStorages, cfg.snapshot, method.globalL0Peer).asResource
+      validators = Validators.make[IO](seedlist)
+      services <- Services
+        .make[IO](
+          p2pClient,
+          sdkServices,
+          storages,
+          sdkResources.client,
+          sdkServices.session,
+          sdk.seedlist,
+          sdk.nodeId,
+          keyPair,
+          cfg,
+          dataApplication,
+          rewards
+        )
+        .asResource
+      programs = Programs.make[IO](
+        keyPair,
+        sdk.nodeId,
+        method.identifier,
+        cfg.globalL0Peer,
+        sdkPrograms,
+        storages,
+        services,
+        p2pClient,
+        services.snapshotContextFunctions
+      )
+      healthChecks <- HealthChecks
+        .make[IO](
+          storages,
+          services,
+          programs,
+          p2pClient,
+          sdkResources.client,
+          sdkServices.session,
+          cfg.healthCheck,
+          sdk.nodeId
+        )
+        .asResource
+      rumorHandler = RumorHandlers.make[IO](storages.cluster, healthChecks.ping, services.localHealthcheck).handlers <+>
+        services.consensus.handler
+      _ <- Daemons
+        .start(storages, services, programs, queues, healthChecks, dataApplication)
+        .asResource
+      api = HttpApi
+        .make[IO](
+          storages,
+          queues,
+          services,
+          programs,
+          healthChecks,
+          keyPair.getPrivate,
+          cfg.environment,
+          sdk.nodeId,
+          BuildInfo.version,
+          cfg.http
+        )
+      _ <- MkHttpServer[IO].newEmber(ServerName("public"), cfg.http.publicHttp, api.publicApp)
+      _ <- MkHttpServer[IO].newEmber(ServerName("p2p"), cfg.http.p2pHttp, api.p2pApp)
+      _ <- MkHttpServer[IO].newEmber(ServerName("cli"), cfg.http.cliHttp, api.cliApp)
+
+      gossipDaemon = GossipDaemon.make[IO](
+        storages.rumor,
+        queues.rumor,
+        storages.cluster,
+        p2pClient.gossip,
+        rumorHandler,
+        validators.rumorValidator,
+        services.localHealthcheck,
+        nodeId,
+        generation,
+        cfg.gossip.daemon,
+        services.collateral
+      )
+
+      _ <- (method match {
+        case _: RunValidator =>
+          gossipDaemon.startAsRegularValidator >>
+            programs.globalL0PeerDiscovery.discoverFrom(cfg.globalL0Peer) >>
+            storages.node.tryModifyState(NodeState.Initial, NodeState.ReadyToJoin)
+
+        case _: RunRollback =>
+          storages.node.tryModifyState(
+            NodeState.Initial,
+            NodeState.RollbackInProgress,
+            NodeState.RollbackDone
+          )(programs.rollback.rollback) >> gossipDaemon.startAsInitialValidator >>
+            services.cluster.createSession >>
+            services.session.createSession >>
+            programs.globalL0PeerDiscovery.discoverFrom(cfg.globalL0Peer) >>
+            storages.node.setNodeState(NodeState.Ready)
+
+        case m: RunGenesis =>
+          storages.node.tryModifyState(
+            NodeState.Initial,
+            NodeState.LoadingGenesis,
+            NodeState.GenesisReady
+          ) {
+            dataApplication
+              .traverse(_.serializedGenesis)
+              .flatMap(programs.genesis.accept(m.genesisPath, _))
+          } >> gossipDaemon.startAsInitialValidator >>
+            services.cluster.createSession >>
+            services.session.createSession >>
+            programs.globalL0PeerDiscovery.discoverFrom(cfg.globalL0Peer) >>
+            storages.node.setNodeState(NodeState.Ready)
+      }).asResource
+    } yield ()
+  }
+}
---
+++ modules/currency-l1/src/main/scala/org/tessellation/currency/l1/CurrencyL1App.scala
@@ -0,0 +1,198 @@
+package org.tessellation.currency.l1
+
+import cats.Applicative
+import cats.effect.IO
+import cats.effect.kernel.Resource
+import cats.syntax.semigroupk._
+
+import scala.concurrent.duration._
+
+import org.tessellation.BuildInfo
+import org.tessellation.currency.l1.cli.method.{Run, RunInitialValidator, RunValidator}
+import org.tessellation.currency.l1.domain.snapshot.programs.CurrencySnapshotProcessor
+import org.tessellation.currency.l1.modules.{Programs, Storages}
+import org.tessellation.currency.schema.currency._
+import org.tessellation.dag.l1.http.p2p.P2PClient
+import org.tessellation.dag.l1.infrastructure.block.rumor.handler.blockRumorHandler
+import org.tessellation.dag.l1.modules._
+import org.tessellation.dag.l1.{DagL1KryoRegistrationIdRange, StateChannel, dagL1KryoRegistrar}
+import org.tessellation.ext.cats.effect.ResourceIO
+import org.tessellation.ext.kryo.{KryoRegistrationId, MapRegistrationId}
+import org.tessellation.schema.cluster.ClusterId
+import org.tessellation.schema.node.NodeState
+import org.tessellation.schema.node.NodeState.SessionStarted
+import org.tessellation.schema.peer.PeerId
+import org.tessellation.sdk.app.{SDK, TessellationIOApp}
+import org.tessellation.sdk.infrastructure.gossip.{GossipDaemon, RumorHandlers}
+import org.tessellation.sdk.resources.MkHttpServer
+import org.tessellation.sdk.resources.MkHttpServer.ServerName
+import org.tessellation.sdk.{SdkOrSharedOrKernelRegistrationIdRange, sdkKryoRegistrar}
+
+import com.monovore.decline.Opts
+import eu.timepit.refined.boolean.Or
+import fs2.Stream
+
+abstract class CurrencyL1App(
+  name: String,
+  header: String,
+  clusterId: ClusterId,
+  version: String
+) extends TessellationIOApp[Run](
+      name,
+      header,
+      clusterId,
+      version = version
+    ) {
+
+  val opts: Opts[Run] = cli.method.opts
+
+  type KryoRegistrationIdRange = SdkOrSharedOrKernelRegistrationIdRange Or DagL1KryoRegistrationIdRange
+
+  val kryoRegistrar: Map[Class[_], KryoRegistrationId[KryoRegistrationIdRange]] =
+    sdkKryoRegistrar.union(dagL1KryoRegistrar)
+
+  def run(method: Run, sdk: SDK[IO]): Resource[IO, Unit] = {
+    import sdk._
+
+    val cfg = method.appConfig
+
+    for {
+      queues <- Queues.make[IO, CurrencyTransaction, CurrencyBlock](sdkQueues).asResource
+      storages <- Storages
+        .make[IO, CurrencyTransaction, CurrencyBlock, CurrencySnapshotStateProof, CurrencyIncrementalSnapshot, CurrencySnapshotInfo](
+          sdkStorages,
+          method.l0Peer,
+          method.globalL0Peer
+        )
+        .asResource
+      validators = Validators
+        .make[IO, CurrencyTransaction, CurrencyBlock, CurrencySnapshotStateProof, CurrencyIncrementalSnapshot, CurrencySnapshotInfo](
+          storages,
+          seedlist
+        )
+      p2pClient = P2PClient.make[IO, CurrencyTransaction, CurrencyBlock](
+        sdkP2PClient,
+        sdkResources.client,
+        currencyPathPrefix = "currency"
+      )
+      services = Services
+        .make[IO, CurrencyTransaction, CurrencyBlock, CurrencySnapshotStateProof, CurrencyIncrementalSnapshot, CurrencySnapshotInfo](
+          storages,
+          storages.lastGlobalSnapshot,
+          storages.globalL0Cluster,
+          validators,
+          sdkServices,
+          p2pClient,
+          cfg
+        )
+      snapshotProcessor = CurrencySnapshotProcessor.make(
+        method.identifier,
+        storages.address,
+        storages.block,
+        storages.lastGlobalSnapshot,
+        storages.lastSnapshot,
+        storages.transaction,
+        sdkServices.globalSnapshotContextFns,
+        sdkServices.currencySnapshotContextFns
+      )
+      programs = Programs
+        .make[IO, CurrencyTransaction, CurrencyBlock, CurrencySnapshotStateProof, CurrencyIncrementalSnapshot, CurrencySnapshotInfo](
+          sdkPrograms,
+          p2pClient,
+          storages,
+          snapshotProcessor
+        )
+      healthChecks <- HealthChecks
+        .make[IO, CurrencyTransaction, CurrencyBlock, CurrencySnapshotStateProof, CurrencyIncrementalSnapshot, CurrencySnapshotInfo](
+          storages,
+          services,
+          programs,
+          p2pClient,
+          sdkResources.client,
+          sdkServices.session,
+          cfg.healthCheck,
+          sdk.nodeId
+        )
+        .asResource
+
+      rumorHandler = RumorHandlers.make[IO](storages.cluster, healthChecks.ping, services.localHealthcheck).handlers <+>
+        blockRumorHandler[IO, CurrencyBlock](queues.peerBlock)
+
+      _ <- Daemons
+        .start(storages, services, healthChecks)
+        .asResource
+
+      api = HttpApi
+        .make[IO, CurrencyTransaction, CurrencyBlock, CurrencySnapshotStateProof, CurrencyIncrementalSnapshot, CurrencySnapshotInfo](
+          storages,
+          queues,
+          keyPair.getPrivate,
+          services,
+          programs,
+          healthChecks,
+          sdk.nodeId,
+          BuildInfo.version,
+          cfg.http
+        )
+      _ <- MkHttpServer[IO].newEmber(ServerName("public"), cfg.http.publicHttp, api.publicApp)
+      _ <- MkHttpServer[IO].newEmber(ServerName("p2p"), cfg.http.p2pHttp, api.p2pApp)
+      _ <- MkHttpServer[IO].newEmber(ServerName("cli"), cfg.http.cliHttp, api.cliApp)
+
+      stateChannel <- StateChannel
+        .make[IO, CurrencyTransaction, CurrencyBlock, CurrencySnapshotStateProof, CurrencyIncrementalSnapshot, CurrencySnapshotInfo](
+          cfg,
+          keyPair,
+          p2pClient,
+          programs,
+          queues,
+          nodeId,
+          services,
+          storages,
+          validators
+        )
+        .asResource
+
+      gossipDaemon = GossipDaemon.make[IO](
+        storages.rumor,
+        queues.rumor,
+        storages.cluster,
+        p2pClient.gossip,
+        rumorHandler,
+        validators.rumorValidator,
+        services.localHealthcheck,
+        nodeId,
+        generation,
+        cfg.gossip.daemon,
+        services.collateral
+      )
+      _ <- {
+        method match {
+          case cfg: RunInitialValidator =>
+            gossipDaemon.startAsInitialValidator >>
+              programs.l0PeerDiscovery.discoverFrom(cfg.l0Peer) >>
+              programs.globalL0PeerDiscovery.discoverFrom(cfg.globalL0Peer) >>
+              storages.node.tryModifyState(NodeState.Initial, NodeState.ReadyToJoin) >>
+              services.cluster.createSession >>
+              services.session.createSession >>
+              storages.node.tryModifyState(SessionStarted, NodeState.Ready)
+
+          case cfg: RunValidator =>
+            gossipDaemon.startAsRegularValidator >>
+              programs.l0PeerDiscovery.discoverFrom(cfg.l0Peer) >>
+              programs.globalL0PeerDiscovery.discoverFrom(cfg.globalL0Peer) >>
+              storages.node.tryModifyState(NodeState.Initial, NodeState.ReadyToJoin)
+        }
+      }.asResource
+      globalL0PeerDiscovery = Stream
+        .awakeEvery[IO](10.seconds)
+        .evalMap { _ =>
+          storages.lastGlobalSnapshot.get.flatMap {
+            _.fold(Applicative[IO].unit) { latestSnapshot =>
+              programs.globalL0PeerDiscovery.discover(latestSnapshot.signed.proofs.map(_.id).map(PeerId._Id.reverseGet))
+            }
+          }
+        }
+      _ <- stateChannel.runtime.merge(globalL0PeerDiscovery).compile.drain.asResource
+    } yield ()
+  }
+}
