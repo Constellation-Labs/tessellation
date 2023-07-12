1614b7c29a88a8f0ada5e113e02eec8174299ad7
Marcin Wadon <mwadon@evojam.com>
2023-07-12 17:02:59 +0200

    Add SecurityProvider to DataApplication context

--- modules/currency-l0/src/main/scala/org/tessellation/currency/l0/node/L0NodeContext.scala
+++ modules/currency-l0/src/main/scala/org/tessellation/currency/l0/node/L0NodeContext.scala
@@ -3,12 +3,14 @@ package org.tessellation.currency.l0.node
 import org.tessellation.currency.dataApplication.L0NodeContext
 import org.tessellation.schema.{GlobalIncrementalSnapshot, GlobalSnapshotInfo}
 import org.tessellation.sdk.domain.snapshot.storage.LastSnapshotStorage
-import org.tessellation.security.Hashed
+import org.tessellation.security.{Hashed, SecurityProvider}
 
 object L0NodeContext {
-  def make[F[_]](
+  def make[F[_]: SecurityProvider](
     lastGlobalSnapshotStorage: LastSnapshotStorage[F, GlobalIncrementalSnapshot, GlobalSnapshotInfo]
   ): L0NodeContext[F] = new L0NodeContext[F] {
     def getLastGlobalSnapshot: F[Option[Hashed[GlobalIncrementalSnapshot]]] = lastGlobalSnapshotStorage.get
+
+    def securityProvider: SecurityProvider[F] = SecurityProvider[F]
   }
 }
