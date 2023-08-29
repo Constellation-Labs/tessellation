59727242bff0f9943981be560d0797d327e83da4
Krystian Pudlik <krystian.pudlik@gmail.com>
2023-08-29 15:50:31 +0200

    Test size

--- modules/core/src/test/scala/org/tessellation/infrastructure/snapshot/GlobalSnapshotSerializationSuite.scala
+++ modules/core/src/test/scala/org/tessellation/infrastructure/snapshot/GlobalSnapshotSerializationSuite.scala
@@ -8,10 +8,13 @@ import cats.syntax.all._
 import org.tessellation.ext.crypto._
 import org.tessellation.ext.kryo._
 import org.tessellation.kryo.KryoSerializer
+import org.tessellation.schema.ID.Id
 import org.tessellation.schema._
 import org.tessellation.sdk.sdkKryoRegistrar
 import org.tessellation.security.hash.Hash
+import org.tessellation.security.hex.Hex
 import org.tessellation.security.signature.Signed
+import org.tessellation.security.signature.signature.{Signature, SignatureProof}
 
 import eu.timepit.refined.auto._
 import fs2.io.file.{Files, Path}
@@ -38,7 +41,25 @@ object GlobalSnapshotSerializationSuite extends MutableIOSuite with Checkers {
   test("snapshot is successfully deserialized and serialized with kryo") { implicit kryo =>
     for {
       storedBytes <- getBytesFromClasspath(kryoFilename)
-      signedSnapshot <- storedBytes.fromBinaryF[Signed[GlobalSnapshot]]
+      signedSnapshot: Signed[GlobalSnapshot] <- storedBytes.fromBinaryF[Signed[GlobalSnapshot]]
+      _ = println(signedSnapshot.proofs)
+      _ = println("") // 128 + 142 single SignatureProof
+      additionalSignature = SignatureProof(
+        Id(
+          Hex(
+            "f0073ae1852b2adee1af9353b5b7b124fd417b862565edfe5c853973e23e313180f6baffa4628a7ea26af7115bf1176ad70415ed8c08dd3c42264286bf4a33a5"
+          )
+        ),
+        Signature(
+          Hex(
+            "30450221009c2fed2fc6ee39781e1bad9e3721f3dce1696b25b2d802bf0c3760573c217b34022054d9144b0418461ff67e371cffe6dedee0d6f8afd492f734c3d8be5a9ddcadcc"
+          )
+        )
+      )
+      proofSizesInBytes <- (signedSnapshot.proofs.toList :+ additionalSignature).traverse(_.toBinaryF)
+      wholeSize <- (signedSnapshot.proofs.toList :+ additionalSignature).toBinaryF.map(_.length)
+      _ = for (a <- proofSizesInBytes)
+        println(wholeSize) // 296
       serializedBytes <- signedSnapshot.toBinaryF
       snapshotHash <- signedSnapshot.value.hashF
     } yield expect.same(serializedBytes, storedBytes).and(expect.same(snapshotHash, expectedHash))
