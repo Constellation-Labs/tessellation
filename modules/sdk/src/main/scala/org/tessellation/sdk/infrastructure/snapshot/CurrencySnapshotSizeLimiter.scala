1de54641b781c39abf542db4088791373afb4a4e
Krystian Pudlik <krystian.pudlik@gmail.com>
2023-08-29 10:27:08 +0200

    CurrencySnapshotSizeLimiter

---
+++ modules/sdk/src/main/scala/org/tessellation/sdk/infrastructure/snapshot/CurrencySnapshotSizeLimiter.scala
@@ -0,0 +1,70 @@
+package org.tessellation.sdk.infrastructure.snapshot
+
+import cats.MonadThrow
+import cats.syntax.applicative._
+import cats.syntax.either._
+import cats.syntax.flatMap._
+import cats.syntax.functor._
+
+import scala.collection.immutable.SortedSet
+
+import org.tessellation.currency.schema.currency.CurrencyIncrementalSnapshot
+import org.tessellation.ext.kryo._
+import org.tessellation.kryo.KryoSerializer
+import org.tessellation.schema.BlockAsActiveTip
+import org.tessellation.syntax.sortedCollection.sortedSetSyntax
+
+import monocle.macros.syntax.lens._
+
+case class SnapshotSizeLimiterResult(
+  snapshot: CurrencyIncrementalSnapshot,
+  oldSizeInBytes: Long,
+  newSizeInBytes: Long,
+  excessBlocks: SortedSet[BlockAsActiveTip]
+)
+
+trait CurrencySnapshotSizeLimiter[F[_]] {
+  def limit(snapshot: CurrencyIncrementalSnapshot, maxSizeInBytes: Long): F[Option[SnapshotSizeLimiterResult]]
+}
+
+object CurrencySnapshotSizeLimiter {
+  def make[F[_]: MonadThrow: KryoSerializer]: CurrencySnapshotSizeLimiter[F] =
+    (snapshot: CurrencyIncrementalSnapshot, maxSizeInBytes: Long) => {
+      type Success = SnapshotSizeLimiterResult
+      type Agg = (CurrencyIncrementalSnapshot, SortedSet[BlockAsActiveTip])
+      type Result = Option[Success]
+
+      snapshot.toBinaryF.flatMap { initialBytes: Array[Byte] =>
+        val oldSize = initialBytes.length
+
+        (snapshot, SortedSet.empty[BlockAsActiveTip]).tailRecM[F, Result] {
+          case (snapshot, rejected) =>
+            println(s"Blocks:  ${snapshot.blocks.size} Rejected: ${rejected.size}")
+            for {
+              bytes: Array[Byte] <- snapshot.toBinaryF
+              size = bytes.length
+              _ = println(s"Size: ${size} (expected: ${maxSizeInBytes})")
+              result <-
+                if (size <= maxSizeInBytes) {
+                  Some(
+                    SnapshotSizeLimiterResult(
+                      snapshot,
+                      oldSize.toLong,
+                      size.toLong,
+                      rejected
+                    )
+                  ).asRight[Agg].pure[F]
+                } else {
+                  snapshot.blocks.toList match {
+                    case toReject :: toLeave =>
+                      (snapshot.focus(_.blocks).replace(toLeave.toSortedSet), rejected + toReject).asLeft[Result].pure[F]
+                    case _ => None.asRight[Agg].pure[F]
+                  }
+                }
+            } yield result
+        }
+
+      }
+
+    }
+}
---
+++ modules/sdk/src/test/scala/org/tessellation/sdk/infrastructure/snapshot/CurrencySnapshotSizeLimiterSuite.scala
@@ -0,0 +1,64 @@
+package org.tessellation.sdk.infrastructure.snapshot
+
+import cats.effect.{IO, Resource}
+import cats.syntax.applicative._
+
+import scala.collection.immutable.SortedSet
+
+import org.tessellation.block.generators.signedBlockGen
+import org.tessellation.currency.schema.currency.{CurrencyIncrementalSnapshot, CurrencySnapshotStateProof}
+import org.tessellation.kryo.KryoSerializer
+import org.tessellation.schema.epoch.EpochProgress
+import org.tessellation.schema.height.{Height, SubHeight}
+import org.tessellation.schema.{BlockAsActiveTip, SnapshotOrdinal, SnapshotTips}
+import org.tessellation.sdk.sdkKryoRegistrar
+import org.tessellation.security.hash.Hash
+import org.tessellation.syntax.sortedCollection.sortedSetSyntax
+
+import eu.timepit.refined.auto._
+import org.scalacheck.Gen
+import weaver.MutableIOSuite
+import weaver.scalacheck.Checkers
+
+object CurrencySnapshotSizeLimiterSuite extends MutableIOSuite with Checkers {
+
+  override type Res = KryoSerializer[IO]
+
+  override def sharedResource: Resource[IO, KryoSerializer[IO]] = KryoSerializer.forAsync[IO](sdkKryoRegistrar)
+
+  private def mkCurrencyIncremental(blocks: Int) = for {
+    blocks <- Gen.listOfN(blocks, signedBlockGen)
+    asActiveTips = blocks.map(BlockAsActiveTip(_, 1L))
+  } yield
+    CurrencyIncrementalSnapshot(
+      SnapshotOrdinal.MinValue,
+      Height.MinValue,
+      SubHeight.MinValue,
+      Hash.empty,
+      asActiveTips.toSortedSet,
+      SortedSet.empty,
+      SnapshotTips(SortedSet.empty, SortedSet.empty),
+      CurrencySnapshotStateProof(Hash.empty, Hash.empty),
+      EpochProgress.MinValue,
+      None
+    )
+
+  test("limiter rejects excess blocks to fit the size limit") { implicit kryo =>
+    val maxSizeInBytes: Long = 50 * 1024
+
+    forall(mkCurrencyIncremental(10)) { snapshot =>
+      for {
+        limiter <- CurrencySnapshotSizeLimiter.make[IO].pure[IO]
+        result <- limiter.limit(snapshot, maxSizeInBytes)
+        succeeded = result match {
+          case Some(result) =>
+            val originalSizeFits = result.oldSizeInBytes <= maxSizeInBytes
+            val newSizeFits = result.newSizeInBytes <= maxSizeInBytes
+            val hasExceededBlocks = result.oldSizeInBytes != result.newSizeInBytes && result.excessBlocks.nonEmpty
+            originalSizeFits || (newSizeFits && hasExceededBlocks)
+          case None => false
+        }
+      } yield expect.same(succeeded, true)
+    }
+  }
+}
