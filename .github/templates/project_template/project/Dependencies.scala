387f6b4194e2a3bcf1442705648a271d206d0992
Marcus Girolneto <marcus.girolneto@gmail.com>
2023-12-13 14:31:23 -0300

    Migrating `github-action-metagraph-test` to tessellation repository

---
+++ .github/templates/project_template/project/Dependencies.scala
@@ -0,0 +1,48 @@
+import sbt._
+
+object Dependencies {
+
+  object V {
+    //Default version of tessellation develop branch
+    val tessellation = "github-action-test-version"
+    val decline = "2.4.1"
+  }
+  def tessellation(artifact: String): ModuleID = "org.constellation" %% s"tessellation-$artifact" % V.tessellation
+
+  def decline(artifact: String = ""): ModuleID =
+    "com.monovore" %% {
+      if (artifact.isEmpty) "decline" else s"decline-$artifact"
+    } % V.decline
+  object Libraries {
+    val tessellationKernel = tessellation("kernel")
+    val tessellationDAGL1 = tessellation("dag-l1")
+    val tessellationNodeShared = tessellation("node-shared")
+    val tessellationShared = tessellation("shared")
+    val tessellationKeytool = tessellation("keytool")
+    val tessellationCurrencyL0 = tessellation("currency-l0")
+    val tessellationCurrencyL1 = tessellation("currency-l1")
+    val requests = "com.lihaoyi" %% "requests" % "0.8.0"
+    val declineCore = decline()
+    val declineEffect = decline("effect")
+    val declineRefined = decline("refined")
+  }
+
+
+  // Scalafix rules
+  val organizeImports = "com.github.liancheng" %% "organize-imports" % "0.5.0"
+
+  object CompilerPlugin {
+
+    val betterMonadicFor = compilerPlugin(
+      "com.olegpy" %% "better-monadic-for" % "0.3.1"
+    )
+
+    val kindProjector = compilerPlugin(
+      ("org.typelevel" % "kind-projector" % "0.13.2").cross(CrossVersion.full)
+    )
+
+    val semanticDB = compilerPlugin(
+      ("org.scalameta" % "semanticdb-scalac" % "4.7.1").cross(CrossVersion.full)
+    )
+  }
+}
---
+++ .github/templates/project_template/project/plugins.sbt
@@ -0,0 +1,11 @@
+addSbtPlugin("io.github.davidgregory084" % "sbt-tpolecat" % "0.4.1")
+addSbtPlugin("io.spray" % "sbt-revolver" % "0.9.1")
+addSbtPlugin("com.github.sbt" % "sbt-native-packager" % "1.9.11")
+addSbtPlugin("org.scalameta" % "sbt-scalafmt" % "2.4.6")
+addSbtPlugin("ch.epfl.scala" % "sbt-scalafix" % "0.10.1")
+addSbtPlugin("ch.epfl.scala" % "sbt-bloop" % "1.5.3")
+addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "1.2.0")
+addSbtPlugin("com.eed3si9n" % "sbt-buildinfo" % "0.11.0")
+addSbtPlugin("com.codecommit" % "sbt-github-packages" % "0.5.3")
+addSbtPlugin("com.timushev.sbt" % "sbt-updates" % "0.6.3")
+addDependencyTreePlugin
