1a45f7ba6398e3d8f087ebee2c44bce59fa68fff
sdavidp <sdpullara@gmail.com>
2024-04-02 15:19:49 -0500

    Removed historical(unused) endpoints

--- modules/node-shared/src/main/scala/org/tessellation/node/shared/infrastructure/snapshot/services/AddressService.scala
+++ modules/node-shared/src/main/scala/org/tessellation/node/shared/infrastructure/snapshot/services/AddressService.scala
@@ -28,18 +28,10 @@ object AddressService {
             (balance, ordinal)
         })
 
-      // INFO: No historical balances can be red from the current state, hence None returned to keep backward compatibility
-      def getBalance(ordinal: SnapshotOrdinal, address: Address): F[Option[(Balance, SnapshotOrdinal)]] =
-        Applicative[F].pure(None)
-
       def getTotalSupply: F[Option[(BigInt, SnapshotOrdinal)]] =
         snapshotStorage.head.map(_.map {
           case (snapshot, state) => calculateTotalSupply(state.balances.values, snapshot.value.ordinal)
         })
-
-      // INFO: No historical balances can be red from the current state, hence None returned to keep backward compatibility
-      def getTotalSupply(ordinal: SnapshotOrdinal): F[Option[(BigInt, SnapshotOrdinal)]] =
-        Applicative[F].pure(None)
 
       def getWalletCount: F[Option[(Int, SnapshotOrdinal)]] =
         snapshotStorage.head.map(_.map {
@@ -49,10 +41,6 @@ object AddressService {
 
             (balance, ordinal)
         })
-
-      // INFO: No historical balances can be red from the current state, hence None returned to keep backward compatibility
-      def getWalletCount(ordinal: SnapshotOrdinal): F[Option[(Int, SnapshotOrdinal)]] =
-        Applicative[F].pure(None)
 
       def getFilteredOutTotalSupply: F[Option[(BigInt, SnapshotOrdinal)]] =
         snapshotStorage.head.map(_.map {
