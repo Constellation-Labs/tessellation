f39aa8b1ec1845d82c552d3c96da46a8ceb2611c
Marcus Vinicius Girolneto Sousa <marcus.girolneto@gmail.com>
2024-10-15 11:52:00 -0300

    feat: extracting spend transactions to schema

---
+++ modules/shared/src/main/scala/io/constellationnetwork/schema/artifact.scala
@@ -0,0 +1,50 @@
+package io.constellationnetwork.schema
+
+import cats.effect.kernel.Async
+import cats.syntax.functor._
+
+import io.constellationnetwork.ext.derevo.ordering
+import io.constellationnetwork.schema.epoch.EpochProgress
+import io.constellationnetwork.schema.swap.{CurrencyId, SwapAmount}
+import io.constellationnetwork.security.Hasher
+import io.constellationnetwork.security.hash.Hash
+
+import derevo.cats.{order, show}
+import derevo.circe.magnolia.{decoder, encoder}
+import derevo.derive
+import eu.timepit.refined.types.numeric.PosLong
+import io.estatico.newtype.macros.newtype
+
+object artifact {
+  @derive(decoder, encoder, order, ordering, show)
+  sealed trait SharedArtifact
+
+  @derive(decoder, encoder, order, ordering, show)
+  sealed trait SpendTransaction extends SharedArtifact
+
+  @derive(decoder, encoder, order, show)
+  @newtype
+  case class SpendTransactionFee(value: PosLong)
+
+  @derive(decoder, encoder, order, show)
+  @newtype
+  case class SpendTransactionReference(hash: Hash)
+
+  object SpendTransactionReference {
+    def of[F[_]: Async](spendTransaction: SpendTransaction)(implicit hasher: Hasher[F]): F[SpendTransactionReference] =
+      hasher.hash(spendTransaction).map(SpendTransactionReference(_))
+  }
+  @derive(decoder, encoder, order, show)
+  case class PendingSpendTransaction(
+    fee: SpendTransactionFee,
+    lastValidEpochProgress: EpochProgress,
+    allowSpendRef: Hash,
+    currency: Option[CurrencyId],
+    amount: SwapAmount
+  ) extends SpendTransaction
+
+  @derive(decoder, encoder, order, show)
+  case class ConcludedSpendTransaction(
+    spendTransactionRef: SpendTransactionReference
+  ) extends SpendTransaction
+}
