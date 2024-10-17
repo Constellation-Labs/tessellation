213d0e47766c715ec435c8ba82819f7a5d79befd
Marcin Wadon <mwadon@evojam.com>
2024-10-17 15:09:20 +0200

    feat: run allow spends runtime

--- modules/dag-l1/src/main/scala/io/constellationnetwork/dag/l1/modules/Queues.scala
+++ modules/dag-l1/src/main/scala/io/constellationnetwork/dag/l1/modules/Queues.scala
@@ -5,10 +5,13 @@ import cats.effect.std.Queue
 import cats.syntax.flatMap._
 import cats.syntax.functor._
 
+import io.constellationnetwork.currency.swap.{ConsensusInput => SwapConsensusInput}
 import io.constellationnetwork.dag.l1.domain.consensus.block.BlockConsensusInput.PeerBlockConsensusInput
+import io.constellationnetwork.node.shared.domain.queue.ViewableQueue
 import io.constellationnetwork.node.shared.modules.SharedQueues
 import io.constellationnetwork.schema.Block
 import io.constellationnetwork.schema.gossip.RumorRaw
+import io.constellationnetwork.schema.swap.SwapTransaction
 import io.constellationnetwork.security.Hashed
 import io.constellationnetwork.security.signature.Signed
 
@@ -18,11 +21,15 @@ object Queues {
     for {
       peerBlockConsensusInputQueue <- Queue.unbounded[F, Signed[PeerBlockConsensusInput]]
       peerBlockQueue <- Queue.unbounded[F, Signed[Block]]
+      swapPeerConsensusInputQueue <- Queue.unbounded[F, Signed[SwapConsensusInput.PeerConsensusInput]]
+      swapTransactionsQueue <- ViewableQueue.make[F, Signed[SwapTransaction]]
     } yield
       new Queues[F] {
         val rumor: Queue[F, Hashed[RumorRaw]] = sharedQueues.rumor
         val peerBlockConsensusInput: Queue[F, Signed[PeerBlockConsensusInput]] = peerBlockConsensusInputQueue
         val peerBlock: Queue[F, Signed[Block]] = peerBlockQueue
+        val swapPeerConsensusInput = swapPeerConsensusInputQueue
+        val swapTransactions = swapTransactionsQueue
       }
 }
 
@@ -30,4 +37,6 @@ sealed abstract class Queues[F[_]] private {
   val rumor: Queue[F, Hashed[RumorRaw]]
   val peerBlockConsensusInput: Queue[F, Signed[PeerBlockConsensusInput]]
   val peerBlock: Queue[F, Signed[Block]]
+  val swapPeerConsensusInput: Queue[F, Signed[SwapConsensusInput.PeerConsensusInput]]
+  val swapTransactions: ViewableQueue[F, Signed[SwapTransaction]]
 }
