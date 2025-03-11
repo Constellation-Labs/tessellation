bea8a66a429ada0c55ac8fa47ad9bb35e2cccba1
Marcus Vinicius Girolneto Sousa <marcus.girolneto@gmail.com>
2025-03-11 16:28:55 -0300

    fix: fixing spend action balance updates

--- modules/node-shared/src/main/scala/io/constellationnetwork/node/shared/domain/swap/SpendActionValidator.scala
+++ modules/node-shared/src/main/scala/io/constellationnetwork/node/shared/domain/swap/SpendActionValidator.scala
@@ -7,8 +7,10 @@ import cats.syntax.all._
 
 import scala.collection.immutable.{SortedMap, SortedSet}
 
+import io.constellationnetwork.node.shared.domain.swap.SpendActionValidator.SpendActionValidationErrorOr
 import io.constellationnetwork.schema.address.Address
 import io.constellationnetwork.schema.artifact.{SpendAction, SpendTransaction}
+import io.constellationnetwork.schema.balance.Balance
 import io.constellationnetwork.schema.swap.AllowSpend
 import io.constellationnetwork.security.Hasher
 import io.constellationnetwork.security.signature.Signed
@@ -16,12 +18,12 @@ import io.constellationnetwork.security.signature.Signed
 import derevo.cats.{eqv, show}
 import derevo.derive
 
-import SpendActionValidator.SpendActionValidationErrorOr
-
 trait SpendActionValidator[F[_]] {
   def validate(
     spendAction: SpendAction,
-    activeAllowSpends: SortedMap[Option[Address], SortedMap[Address, SortedSet[Signed[AllowSpend]]]]
+    activeAllowSpends: SortedMap[Option[Address], SortedMap[Address, SortedSet[Signed[AllowSpend]]]],
+    globalBalances: Map[Address, Balance],
+    currencyId: Address
   ): F[SpendActionValidationErrorOr[SpendAction]]
 }
 
@@ -29,40 +31,75 @@ object SpendActionValidator {
   def make[F[_]: Async: Hasher]: SpendActionValidator[F] = new SpendActionValidator[F] {
     def validate(
       spendAction: SpendAction,
-      activeAllowSpends: SortedMap[Option[Address], SortedMap[Address, SortedSet[Signed[AllowSpend]]]]
-    ): F[SpendActionValidationErrorOr[SpendAction]] =
+      activeAllowSpends: SortedMap[Option[Address], SortedMap[Address, SortedSet[Signed[AllowSpend]]]],
+      globalBalances: Map[Address, Balance],
+      currencyId: Address
+    ): F[SpendActionValidationErrorOr[SpendAction]] = {
+      val currencyIdBalance = globalBalances.getOrElse(currencyId, Balance.empty)
       for {
-        input <- validateSpendTx(spendAction.input, activeAllowSpends)
-        output <- validateSpendTx(spendAction.output, activeAllowSpends)
+        input <- validateSpendTx(spendAction.input, activeAllowSpends, currencyIdBalance)
+        output <- validateSpendTx(spendAction.output, activeAllowSpends, currencyIdBalance)
       } yield (input *> output).as(spendAction)
+    }
 
     def validateSpendTx(
       tx: SpendTransaction,
-      activeAllowSpends: SortedMap[Option[Address], SortedMap[Address, SortedSet[Signed[AllowSpend]]]]
+      activeAllowSpends: SortedMap[Option[Address], SortedMap[Address, SortedSet[Signed[AllowSpend]]]],
+      currencyIdBalance: Balance
     ): F[SpendActionValidationErrorOr[SpendTransaction]] =
       activeAllowSpends
         .get(tx.currency.map(_.value))
-        .map(validateAllowSpendRef(tx, _))
+        .map(validateAllowSpendRef(tx, _, currencyIdBalance))
         .getOrElse(
           Applicative[F].pure(NoActiveAllowSpends(s"Currency ${tx.currency} not found in active allow spends").invalidNec[SpendTransaction])
         )
 
     def validateAllowSpendRef(
       spendTransaction: SpendTransaction,
-      activeAllowSpends: SortedMap[Address, SortedSet[Signed[AllowSpend]]]
+      activeAllowSpends: SortedMap[Address, SortedSet[Signed[AllowSpend]]],
+      currencyIdBalance: Balance
     ): F[SpendActionValidationErrorOr[SpendTransaction]] =
       spendTransaction.allowSpendRef match {
         case Some(allowSpendRef) =>
-          activeAllowSpends
-            .get(spendTransaction.destination)
-            .traverse(_.toList.traverse(_.toHashed).map(_.exists(_.hash === allowSpendRef)))
-            .map(_.getOrElse(false))
-            .ifF(
-              spendTransaction.validNec[SpendActionValidationError],
-              InvalidDestinationAddress(s"Destination address ${spendTransaction.destination} not found in active allow spends")
-                .invalidNec[SpendTransaction]
-            )
-        case _ => spendTransaction.validNec[SpendActionValidationError].pure[F]
+          activeAllowSpends.toList.traverse {
+            case (_, hashedAllowSpends) =>
+              hashedAllowSpends.toList.traverse(_.toHashed).map { hashedList =>
+                hashedList.map(hashed => hashed.hash -> hashed.signed)
+              }
+          }
+            .map(_.flatten.toMap)
+            .map { allowSpendHashes =>
+              allowSpendHashes.get(allowSpendRef) match {
+                case None =>
+                  AllowSpendNotFound(
+                    s"Allow spend $allowSpendRef not found in currency active allow spends"
+                  ).invalidNec[SpendTransaction]
+
+                case Some(signedAllowSpend) =>
+                  if (signedAllowSpend.currency =!= spendTransaction.currency)
+                    InvalidCurrency(
+                      s"Currency mismatch: expected ${signedAllowSpend.currency}, found ${spendTransaction.currency}"
+                    ).invalidNec[SpendTransaction]
+                  else if (signedAllowSpend.destination =!= spendTransaction.destination)
+                    InvalidDestinationAddress(
+                      s"Destination address ${spendTransaction.destination} not found in active allow spends"
+                    ).invalidNec[SpendTransaction]
+                  else if (signedAllowSpend.amount.value.value < spendTransaction.amount.value.value)
+                    SpendAmountGreaterThanAllowed(
+                      s"Spend amount: ${spendTransaction.amount} greater than allowed: ${signedAllowSpend.amount}"
+                    ).invalidNec[SpendTransaction]
+                  else
+                    spendTransaction.validNec[SpendActionValidationError]
+              }
+            }
+
+        case None =>
+          if (spendTransaction.amount.value.value > currencyIdBalance.value.value)
+            (NotEnoughCurrencyIdBalance(
+              s"Spend amount: ${spendTransaction.amount} greater than currencyId balance: $currencyIdBalance"
+            ): SpendActionValidationError).invalidNec[SpendTransaction].pure[F]
+          else
+            spendTransaction.validNec[SpendActionValidationError].pure[F]
       }
   }
 
@@ -70,6 +107,10 @@ object SpendActionValidator {
   sealed trait SpendActionValidationError
   case class NoActiveAllowSpends(error: String) extends SpendActionValidationError
   case class InvalidDestinationAddress(error: String) extends SpendActionValidationError
+  case class AllowSpendNotFound(error: String) extends SpendActionValidationError
+  case class InvalidCurrency(error: String) extends SpendActionValidationError
+  case class SpendAmountGreaterThanAllowed(error: String) extends SpendActionValidationError
+  case class NotEnoughCurrencyIdBalance(error: String) extends SpendActionValidationError
 
   type SpendActionValidationErrorOr[A] = ValidatedNec[SpendActionValidationError, A]
 }
