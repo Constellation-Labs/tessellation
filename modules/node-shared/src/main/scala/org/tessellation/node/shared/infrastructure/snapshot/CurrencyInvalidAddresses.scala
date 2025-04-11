2637302f8c2482ef1590fe47fa0f678c983d7627
Marcus Vinicius Girolneto Sousa <marcus.girolneto@gmail.com>
2025-04-11 14:36:58 -0300

    fix: adding invalid balances removal for metagraphs

---
+++ modules/node-shared/src/main/scala/org/tessellation/node/shared/infrastructure/snapshot/CurrencyInvalidAddresses.scala
@@ -0,0 +1,27 @@
+package org.tessellation.node.shared.infrastructure.snapshot
+
+import cats.syntax.all._
+
+import scala.collection.immutable.SortedMap
+
+import org.tessellation.schema.SnapshotOrdinal
+import org.tessellation.schema.address.Address
+import org.tessellation.schema.balance.Balance
+
+import eu.timepit.refined.auto._
+import eu.timepit.refined.types.numeric.NonNegLong
+
+object CurrencyInvalidAddresses {
+  case class AddressesInvalidAtOrdinal(
+    snapshotOrdinal: SnapshotOrdinal,
+    filterFunction: SortedMap[Address, Balance] => SortedMap[Address, Balance]
+  )
+
+  val metagraphIdMainnet: Address = Address("DAG7ChnhUF7uKgn8tXy45aj4zn9AFuhaZr8VXY43")
+  val metagraphsInvalidAddresses: Map[Address, AddressesInvalidAtOrdinal] = Map(
+    metagraphIdMainnet -> AddressesInvalidAtOrdinal(
+      SnapshotOrdinal(NonNegLong.unsafeFrom(1268980)),
+      currentBalances => currentBalances.filterNot { case (_, balance) => balance.value.value === (1L * 1e8).toLong }
+    )
+  )
+}
