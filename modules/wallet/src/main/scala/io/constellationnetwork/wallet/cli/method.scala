de345521aab9cc20fe8f596697b7d5f389198e74
ryle-goehausen <ryle@goehausen.org>
2025-04-28 11:33:37 -0700

    feat: wallet methods for delegated stake

--- modules/wallet/src/main/scala/io/constellationnetwork/wallet/cli/method.scala
+++ modules/wallet/src/main/scala/io/constellationnetwork/wallet/cli/method.scala
@@ -26,7 +26,7 @@ object method {
 
   object ShowAddress extends WithOpts[ShowAddress] {
 
-    val opts = Opts.subcommand("show-address", "Shows address") {
+    val opts: Opts[ShowAddress] = Opts.subcommand("show-address", "Shows address") {
       Opts(ShowAddress())
     }
   }
@@ -35,7 +35,7 @@ object method {
 
   object ShowId extends WithOpts[ShowId] {
 
-    val opts =
+    val opts: Opts[ShowId] =
       Opts.subcommand("show-id", "Shows address") {
         Opts(ShowId())
       }
@@ -45,7 +45,7 @@ object method {
 
   object ShowPublicKey extends WithOpts[ShowPublicKey] {
 
-    val opts = Opts.subcommand("show-public-key", "Shows public key") {
+    val opts: Opts[ShowPublicKey] = Opts.subcommand("show-public-key", "Shows public key") {
       Opts(ShowPublicKey())
     }
   }
@@ -75,6 +75,122 @@ object method {
         Opts.option[Path]("prevTxPath", "Path to previously created transaction file", "p").orNone,
         Opts.option[Path]("nextTxPath", "Path where next transaction should be created", "f")
       ).mapN(CreateTransaction.apply)
+    }
+  }
+
+  case class CreateTokenLock(
+    fee: TransactionFee,
+    amount: TransactionAmount,
+    parent: Option[Path],
+    currencyId: Option[Address],
+    unlockEpoch: Option[Long]
+  ) extends CliMethod
+
+  object CreateTokenLock extends WithOpts[CreateTokenLock] {
+
+    val opts: Opts[CreateTokenLock] = Opts.subcommand("create-token-lock", "Creates token lock") {
+      (
+        Opts.option[TransactionFee]("fee", "Token lock fee").withDefault(TransactionFee(NonNegLong(0L))),
+        (
+          Opts.option[TransactionAmount]("amount", "Amount of DAG to lock", "a"),
+          Opts.flag("normalized", "Use to mark that amount is already normalized", "n").orFalse
+        ).tupled.mapValidated {
+          case (amount, normalized) =>
+            if (normalized) amount.validNel
+            else PosLong.from(amount.coerce * 1e8.toLong).map(_.coerce[TransactionAmount]).toValidatedNel
+        },
+        Opts
+          .option[Path](
+            "parent",
+            "Parent tx hash and ordinal in path to load from, " +
+              "formatted as API response",
+            "p"
+          )
+          .orNone,
+        Opts
+          .option[Address](
+            "currencyId",
+            "Address for metagraph currency id, " +
+              "default to none for DAG",
+            "c"
+          )
+          .orNone,
+        Opts.option[Long]("unlockEpoch", "Epoch to unlock, defaults to infinite", "u").orNone
+      ).mapN(CreateTokenLock.apply)
+    }
+  }
+
+  case class CreateDelegatedStake(
+    nodeId: Option[String],
+    fee: TransactionFee,
+    amount: TransactionAmount,
+    parent: Option[Path],
+    tokenLockRef: String
+  ) extends CliMethod
+
+  object CreateDelegatedStake extends WithOpts[CreateDelegatedStake] {
+
+    val opts: Opts[CreateDelegatedStake] = Opts.subcommand("create-delegated-stake", "Creates delegated lock") {
+      (
+        Opts.option[String]("nodeId", "Id of node to stake, defaults to self").orNone,
+        Opts.option[TransactionFee]("fee", "Delegated stake fee").withDefault(TransactionFee(NonNegLong(0L))),
+        (
+          Opts.option[TransactionAmount]("amount", "Amount of DAG to stake, should be same as token lock amount", "a"),
+          Opts.flag("normalized", "Use to mark that amount is already normalized", "n").orFalse
+        ).tupled.mapValidated {
+          case (amount, normalized) =>
+            if (normalized) amount.validNel
+            else PosLong.from(amount.coerce * 1e8.toLong).map(_.coerce[TransactionAmount]).toValidatedNel
+        },
+        Opts
+          .option[Path](
+            "parent",
+            "Parent tx hash and ordinal in path to load from, " +
+              "formatted as API response",
+            "p"
+          )
+          .orNone,
+        Opts.option[String]("token-lock", "Hash reference to active token lock")
+      ).mapN(CreateDelegatedStake.apply)
+    }
+  }
+
+  case class WithdrawDelegatedStake(
+    stakeRef: String
+  ) extends CliMethod
+
+  object WithdrawDelegatedStake extends WithOpts[WithdrawDelegatedStake] {
+    val opts: Opts[WithdrawDelegatedStake] = Opts.subcommand("withdraw-delegated-stake", "Withdraw delegated lock") {
+      Opts
+        .option[String]("stake-ref", "Hash ref to active delegated stake")
+        .map(WithdrawDelegatedStake.apply)
+    }
+  }
+  case class CreateUpdateNodeParameters(
+    rewardFraction: Double,
+    name: String,
+    description: String,
+    parent: Option[Path]
+  ) extends CliMethod
+
+  object CreateUpdateNodeParameters extends WithOpts[CreateUpdateNodeParameters] {
+
+    val opts: Opts[CreateUpdateNodeParameters] = Opts.subcommand("create-node-params", "Creates node parameters request") {
+      (
+        Opts
+          .option[Double]("reward-fraction", "Percent reward to node from delegated stake")
+          .withDefault(0.05),
+        Opts.option[String]("name", "Node name").withDefault("node_name"),
+        Opts.option[String]("description", "Node name").withDefault("node_description"),
+        Opts
+          .option[Path](
+            "parent",
+            "Parent tx hash and ordinal in path to load from, " +
+              "formatted as API response",
+            "p"
+          )
+          .orNone
+      ).mapN(CreateUpdateNodeParameters.apply)
     }
   }
 
@@ -126,4 +242,8 @@ object method {
       .orElse(CreateOwnerSigningMessage.opts)
       .orElse(CreateStakingSigningMessage.opts)
       .orElse(MergeSigningMessages.opts)
+      .orElse(CreateUpdateNodeParameters.opts)
+      .orElse(CreateTokenLock.opts)
+      .orElse(CreateDelegatedStake.opts)
+      .orElse(WithdrawDelegatedStake.opts)
 }
