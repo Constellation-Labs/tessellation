37befe5b650d298fe5cc12e078a72fb9036104be
Oleg Aleksandrov <oleg.v.aleksandrov@gmail.com>
2025-05-15 22:42:05 +0100

    feat: add operation for refined types non neg long and epoch progress

--- modules/shared/src/main/scala/io/constellationnetwork/schema/epoch.scala
+++ modules/shared/src/main/scala/io/constellationnetwork/schema/epoch.scala
@@ -1,10 +1,15 @@
 package io.constellationnetwork.schema
 
 import cats.Order._
+import cats.implicits.catsSyntaxEitherId
 import cats.kernel.{Next, Order, PartialOrder}
 import cats.syntax.semigroup._
 
+import scala.util.control.NoStackTrace
+
 import io.constellationnetwork.ext.derevo.ordering
+import io.constellationnetwork.schema.NonNegLongOps._
+import io.constellationnetwork.schema.epoch.EpochProgress.{EpochOverflow, EpochUnderflow}
 
 import derevo.cats.{eqv, order, show}
 import derevo.circe.magnolia.{decoder, encoder}
@@ -18,7 +23,19 @@ object epoch {
 
   @derive(eqv, encoder, decoder, show, order, ordering, derevo.cats.semigroup)
   @newtype
-  case class EpochProgress(value: NonNegLong)
+  case class EpochProgress(value: NonNegLong) {
+    def plus(that: EpochProgress): Either[EpochOverflow.type, EpochProgress] =
+      value.plus(that.value) match {
+        case Left(_)      => EpochOverflow.asLeft
+        case Right(value) => EpochProgress(value).asRight
+      }
+
+    def minus(that: EpochProgress): Either[EpochUnderflow.type, EpochProgress] =
+      value.minus(that.value) match {
+        case Left(_)      => EpochUnderflow.asLeft
+        case Right(value) => EpochProgress(value).asRight
+      }
+  }
 
   object EpochProgress {
     val MinValue: EpochProgress = EpochProgress(NonNegLong.MinValue)
@@ -28,6 +45,11 @@ object epoch {
       def next(a: EpochProgress): EpochProgress = EpochProgress(a.value |+| NonNegLong(1L))
       def partialOrder: PartialOrder[EpochProgress] = Order[EpochProgress]
     }
+
+    @derive(eqv, show, encoder, decoder)
+    sealed trait EpochArithmeticError extends NoStackTrace
+    case object EpochOverflow extends EpochArithmeticError
+    case object EpochUnderflow extends EpochArithmeticError
   }
 
 }
