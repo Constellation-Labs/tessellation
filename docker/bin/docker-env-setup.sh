3087f4bdc1702d2c2ba761b69336f944afb8e7a6
ryle-goehausen <ryle@goehausen.org>
2025-05-16 14:22:09 -0400

    testing

--- docker/bin/docker-env-setup.sh
+++ docker/bin/docker-env-setup.sh
@@ -1,3 +1,5 @@
+export DAG_L0_PORT_PREFIX=90
+export DAG_L1_PORT_PREFIX=91
 
 export GL0_GENERATED_WALLET_PEER_ID=$(cat ./nodes/0/peer_id)
 echo "Generated GL0 wallet peer id $GL0_GENERATED_WALLET_PEER_ID"
@@ -13,9 +15,11 @@ CL_LOCAL_MODE=true
 CL_L0_PEER_HTTP_HOST=${NET_PREFIX}.10
 CL_DAG_L1_JOIN_IP=${NET_PREFIX}.20
 CL_DAG_L0_JOIN_IP=${NET_PREFIX}.10
-CL_L0_PEER_HTTP_PORT=9000
+CL_L0_PEER_HTTP_PORT=${DAG_L0_PORT_PREFIX}00
 NET_PREFIX=${NET_PREFIX}
 CL_DOCKER_BIND_INTERFACE=${CL_DOCKER_BIND_INTERFACE}
+CL_DAG_L1_JOIN_PORT=${DAG_L1_PORT_PREFIX}01
+CL_DAG_L0_JOIN_PORT=${DAG_L0_PORT_PREFIX}01
 EOF
 
 echo "CL_L0_PEER_ID=$GL0_GENERATED_WALLET_PEER_ID" >> ./nodes/.env
@@ -25,6 +29,7 @@ echo "CL_DAG_L0_JOIN_ID=$GL0_GENERATED_WALLET_PEER_ID" >> ./nodes/.env
 for i in 0 1 2; do
   cp ./nodes/.env ./nodes/$i/.env
   cp ./nodes/.envrc ./nodes/$i/.envrc
+done
 
 
 cp ./.github/config/genesis.csv ./nodes/0/genesis.csv
@@ -57,15 +62,13 @@ echo "CL_GENESIS_FILE=./genesis.csv" >> .env
 # echo "L1_CL_P2P_HTTP_PORT=9011" >> .env
 # echo "L1_CL_CLI_HTTP_PORT=9012" >> .env
 # # External ports
-# echo "CL_DAG_L1_PUBLIC_PORT=9010" >> .env
-# echo "CL_DAG_L1_P2P_PORT=9011" >> .env
-# echo "CL_DAG_L1_CLI_PORT=9012" >> .env
+# echo "CL_DOCKER_EXTERNAL_L1_PUBLIC=9010" >> .env
+# echo "CL_DOCKER_EXTERNAL_L1_P2P=9011" >> .env
+# echo "CL_DOCKER_EXTERNAL_L1_CLI=9012" >> .env
 
 cd ../../
 
 
-export DAG_L0_PORT_PREFIX=90
-export DAG_L1_PORT_PREFIX=91
 
 for i in 0 1 2; do
   cd ./nodes/$i
@@ -87,11 +90,11 @@ for i in 0 1 2; do
 
   # External ports
   echo "CL_DOCKER_EXTERNAL_L0_PUBLIC=${L0_PORT}0" >> .env
-  echo "CL_DAG_L1_PUBLIC_PORT=${L1_PORT}0" >> .env
+  echo "CL_DOCKER_EXTERNAL_L1_PUBLIC=${L1_PORT}0" >> .env
   echo "CL_DOCKER_EXTERNAL_L0_P2P=${L0_PORT}1" >> .env
-  echo "CL_DAG_L1_P2P_PORT=${L1_PORT}1" >> .env
+  echo "CL_DOCKER_EXTERNAL_L1_P2P=${L1_PORT}1" >> .env
   echo "CL_DOCKER_EXTERNAL_L0_CLI=${L0_PORT}2" >> .env
-  echo "CL_DAG_L1_CLI_PORT=${L1_PORT}2" >> .env
+  echo "CL_DOCKER_EXTERNAL_L1_CLI=${L1_PORT}2" >> .env
 
   # These are only required on systems that implement docker with a host networking bridge
   # Port conflicts cause it to fail with external networks that re-use ports
@@ -102,7 +105,7 @@ for i in 0 1 2; do
   echo "L1_CL_PUBLIC_HTTP_PORT=${L1_PORT}0" >> .env
   echo "L1_CL_P2P_HTTP_PORT=${L1_PORT}1" >> .env
   echo "L1_CL_CLI_HTTP_PORT=${L1_PORT}2" >> .env
-  echo "CL_DOCKER_L1_JOIN_DELAY=$((i*30))" >> .env
+  echo "CL_DOCKER_L1_JOIN_DELAY=$((i*10 + 60))" >> .env
 
   cd ../../
 done
