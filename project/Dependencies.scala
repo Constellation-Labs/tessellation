80cc8f9c4a6dee5784cb18dfc7be10772c312580
Scala Steward <scala_steward@virtuslab.com>
2025-05-28 16:23:06 +0000

    Update ciris, ciris-enumeratum, ... to 3.8.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
     val catsEffect = "3.4.2"
     val catsRetry = "3.1.0"
     val circe = "0.14.3"
-    val ciris = "3.0.0"
+    val ciris = "3.8.0"
     val comcast = "3.2.0"
     val decline = "2.4.1"
     val derevo = "0.13.0"
