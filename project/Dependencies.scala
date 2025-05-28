b8534331f2c9dead13d86f42620c375618d20c40
Scala Steward <scala_steward@virtuslab.com>
2025-05-28 16:28:39 +0000

    Update cats-core, cats-laws to 2.13.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
     val bouncyCastle = "1.70"
     val betterFiles = "3.9.1"
     val brotli4j = "1.12.0"
-    val cats = "2.9.0"
+    val cats = "2.13.0"
     val catsEffect = "3.4.2"
     val catsRetry = "3.1.0"
     val circe = "0.14.3"
