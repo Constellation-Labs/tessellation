21d850d2305c582a2089792a5cef89746e8a9c03
Ryle Goehausen <32917835+ryle-goehausen@users.noreply.github.com>
2025-07-08 15:33:30 -0700

    test: override jar or version hash (#1250)

--- modules/node-shared/src/main/scala/io/constellationnetwork/node/shared/app/TessellationIOApp.scala
+++ modules/node-shared/src/main/scala/io/constellationnetwork/node/shared/app/TessellationIOApp.scala
@@ -149,7 +149,10 @@ abstract class TessellationIOApp[A <: CliMethod](
                                     for {
                                       _ <- logger.info(s"Self peerId: $selfId").asResource
                                       _generation <- Generation.make[IO].asResource
-                                      versionHash <- _hasherSelector.withCurrent(_.hash(version)).asResource
+                                      versionHash <- _hasherSelector
+                                        .withCurrent(_.hash(version))
+                                        .asResource
+                                        .map(x => sys.env.get("CL_VERSION_HASH").map(Hash(_)).getOrElse(x))
                                       _seedlist <- loadSeedlist("Seedlist", method.seedlistPath).asResource
                                       _l0Seedlist <- loadSeedlist("l0Seedlist", method.l0SeedlistPath).asResource
                                       _prioritySeedlist <- loadSeedlist("prioritySeedlist", method.prioritySeedlistPath).asResource
@@ -280,6 +283,7 @@ abstract class TessellationIOApp[A <: CliMethod](
         case _ if currentEnv === Dev =>
           logger.warn(s"Using mock value for dev environment.") >> Hash.empty.pure[IO]
       }
+      .map(x => sys.env.get("CL_JAR_HASH").map(Hash(_)).getOrElse(x))
 
   private def loadKeyPair[F[_]: Async: SecurityProvider](
     keyStore: StorePath,
