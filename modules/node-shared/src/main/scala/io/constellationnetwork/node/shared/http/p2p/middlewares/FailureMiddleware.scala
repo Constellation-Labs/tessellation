e8ac59534e0dc4b2730f85278580d00dec77968a
ryle-goehausen <ryle@goehausen.org>
2025-07-08 22:39:57 -0700

    failure middleware

--- modules/node-shared/src/main/scala/io/constellationnetwork/node/shared/http/p2p/middlewares/FailureMiddleware.scala
+++ modules/node-shared/src/main/scala/io/constellationnetwork/node/shared/http/p2p/middlewares/FailureMiddleware.scala
@@ -1,2 +1,39 @@
 package io.constellationnetwork.node.shared.http.p2p.middlewares
 
+import cats.data.{Kleisli, OptionT}
+import cats.effect.Async
+import cats.effect.kernel.Clock
+import cats.effect.syntax.all._
+import cats.implicits._
+
+import scala.concurrent.duration._
+
+import org.http4s.HttpRoutes
+
+object FailureMiddleware {
+
+  def failureSimulatorFeatureCheck[F[_]: Async](envVarName: String): F[Unit] =
+    Async[F].delay {
+      sys.env.get(envVarName).map(_.toLong)
+    }.flatMap {
+      case Some(failTime) =>
+        Clock[F].monotonic.flatMap { currentTime =>
+          val currentTimeSeconds = currentTime.toSeconds
+          if (currentTimeSeconds > failTime) {
+            Async[F].sleep(300.seconds)
+          } else {
+            Async[F].unit
+          }
+        }
+      case None =>
+        Async[F].unit
+    }
+
+  def withFailureSimulator[F[_]: Async](envVarName: String): HttpRoutes[F] => HttpRoutes[F] = { routes =>
+    Kleisli { req =>
+      OptionT.liftF(failureSimulatorFeatureCheck[F](envVarName)).flatMap { _ =>
+        routes(req)
+      }
+    }
+  }
+}
