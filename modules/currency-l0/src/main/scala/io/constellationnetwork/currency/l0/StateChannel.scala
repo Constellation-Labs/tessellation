87601924b5c8185357c661db583abf65f4e2d428
Marcus Vinicius Girolneto Sousa <marcus.girolneto@gmail.com>
2025-08-12 08:59:54 -0300

    chore: removing unused logs

--- modules/currency-l0/src/main/scala/io/constellationnetwork/currency/l0/StateChannel.scala
+++ modules/currency-l0/src/main/scala/io/constellationnetwork/currency/l0/StateChannel.scala
@@ -116,7 +116,12 @@ object StateChannel {
         } yield ()
 
       def conditionallyTriggerEvent(shouldTrigger: Boolean) =
+        if (shouldTrigger) {
+          logger.info("Forcing event trigger due to conditions met") >>
+            enqueueConsensusEventFn(ForceEventTrigger()).run()
+        } else {
           ().pure[F]
+        }
 
       def sendGlobalSnapshotSyncConsensusEvent(snapshot: Hashed[GlobalIncrementalSnapshot])(implicit hs: Hasher[F]) = {
         val selfPeerId = selfKeyPair.getPublic.toId.toPeerId
