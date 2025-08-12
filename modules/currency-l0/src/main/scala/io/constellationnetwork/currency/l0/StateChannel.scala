7913d10ac7f5b4562da2f44315fad75296698501
Marcus Vinicius Girolneto Sousa <marcus.girolneto@gmail.com>
2025-08-12 08:26:48 -0300

    fix: fixing last sync (#1293)

--- modules/currency-l0/src/main/scala/io/constellationnetwork/currency/l0/StateChannel.scala
+++ modules/currency-l0/src/main/scala/io/constellationnetwork/currency/l0/StateChannel.scala
@@ -116,12 +116,7 @@ object StateChannel {
         } yield ()
 
       def conditionallyTriggerEvent(shouldTrigger: Boolean) =
-        if (shouldTrigger) {
-          logger.info("Forcing event trigger due to conditions met") >>
-            enqueueConsensusEventFn(ForceEventTrigger()).run()
-        } else {
           ().pure[F]
-        }
 
       def sendGlobalSnapshotSyncConsensusEvent(snapshot: Hashed[GlobalIncrementalSnapshot])(implicit hs: Hasher[F]) = {
         val selfPeerId = selfKeyPair.getPublic.toId.toPeerId
