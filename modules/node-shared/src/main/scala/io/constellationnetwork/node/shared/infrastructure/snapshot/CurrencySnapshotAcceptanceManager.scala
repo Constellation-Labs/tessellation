85ed3163abaef0fa9a148b1e81e759b09d937131
Marcus Vinicius Girolneto Sousa <marcus.girolneto@gmail.com>
2025-08-28 17:49:54 -0300

    fix: fixing spend actions combine (#1311)

--- modules/node-shared/src/main/scala/io/constellationnetwork/node/shared/infrastructure/snapshot/CurrencySnapshotAcceptanceManager.scala
+++ modules/node-shared/src/main/scala/io/constellationnetwork/node/shared/infrastructure/snapshot/CurrencySnapshotAcceptanceManager.scala
@@ -230,6 +230,8 @@ object CurrencySnapshotAcceptanceManager {
         .getOrElse(environment, SnapshotOrdinal.MinValue)
       updatedLastSyncGlobalFromPeersInConsensus = fieldsAddedOrdinals.updatedLastSyncGlobalFromPeersInConsensus
         .getOrElse(environment, SnapshotOrdinal.MinValue)
+      updatingCombineFunctionSpendActions = fieldsAddedOrdinals.updatingCombineFunctionSpendActions
+        .getOrElse(environment, SnapshotOrdinal.MinValue)
 
       acceptanceBlocksResult <- acceptBlocks(
         blocksForAcceptance,
@@ -386,7 +388,9 @@ object CurrencySnapshotAcceptanceManager {
         getGlobalSnapshotByOrdinal,
         metagraphId,
         lastUnsyncGlobalSnapshotInfo,
-        snapshotOrdinal
+        snapshotOrdinal,
+        lastUnsyncGlobalSnapshot.ordinal,
+        updatingCombineFunctionSpendActions
       )
 
       metagraphIdSpendTransactions = globalSnapshotsSpendActions.flatMap {
@@ -650,7 +654,9 @@ object CurrencySnapshotAcceptanceManager {
       getGlobalSnapshotByOrdinal: SnapshotOrdinal => F[Option[Hashed[GlobalIncrementalSnapshot]]],
       currencyId: Address,
       lastGlobalSnapshotInfo: GlobalSnapshotInfo,
-      currentCurrencySnapshotOrdinal: SnapshotOrdinal
+      currentCurrencySnapshotOrdinal: SnapshotOrdinal,
+      lastUnsyncGlobalSnapshotOrdinal: SnapshotOrdinal,
+      updatedLastSyncGlobalFromPeersInConsensus: SnapshotOrdinal
     ): F[(SortedMap[Address, List[SpendAction]], SortedSet[SnapshotOrdinal])] = {
       val emptySpendActions = SortedMap.empty[Address, List[SpendAction]]
       val emptyProcessedGlobalSnapshots = SortedSet.empty[SnapshotOrdinal]
@@ -686,7 +692,9 @@ object CurrencySnapshotAcceptanceManager {
                       spendActions <- processUnappliedOrdinals(
                         globalOrdinalsToProcess,
                         lastGlobalSnapshots,
-                        getGlobalSnapshotByOrdinal
+                        getGlobalSnapshotByOrdinal,
+                        lastUnsyncGlobalSnapshotOrdinal,
+                        updatedLastSyncGlobalFromPeersInConsensus
                       )
                       _ <- globalSnapshotsAlreadyProcessed.update { current =>
                         val currentMetagraphProcessedOrdinals = current.getOrElse(currencyId, Map.empty)
@@ -717,7 +725,9 @@ object CurrencySnapshotAcceptanceManager {
     private def processUnappliedOrdinals(
       unappliedOrdinals: Set[SnapshotOrdinal],
       lastGlobalSnapshots: List[Hashed[GlobalIncrementalSnapshot]],
-      getGlobalSnapshotByOrdinal: SnapshotOrdinal => F[Option[Hashed[GlobalIncrementalSnapshot]]]
+      getGlobalSnapshotByOrdinal: SnapshotOrdinal => F[Option[Hashed[GlobalIncrementalSnapshot]]],
+      lastUnsyncGlobalSnapshotOrdinal: SnapshotOrdinal,
+      updatedLastSyncGlobalFromPeersInConsensus: SnapshotOrdinal
     ): F[SortedMap[Address, List[SpendAction]]] = {
       val snapshotCache = lastGlobalSnapshots.map(s => s.ordinal -> s).toMap
       val (cached, missing) = unappliedOrdinals.partition(snapshotCache.contains)
@@ -730,15 +740,25 @@ object CurrencySnapshotAcceptanceManager {
           .map(_.spendActions.getOrElse(SortedMap.empty[Address, List[SpendAction]]))
       }
 
-      fetchMissing.map(fromFetched => combineSpendActions(fromCache ++ fromFetched))
+      fetchMissing.map(fromFetched =>
+        combineSpendActions(fromCache ++ fromFetched, lastUnsyncGlobalSnapshotOrdinal, updatedLastSyncGlobalFromPeersInConsensus)
+      )
     }
 
     private def combineSpendActions(
-      spendActionsList: List[SortedMap[Address, List[SpendAction]]]
+      spendActionsList: List[SortedMap[Address, List[SpendAction]]],
+      lastUnsyncGlobalSnapshotOrdinal: SnapshotOrdinal,
+      updatedLastSyncGlobalFromPeersInConsensus: SnapshotOrdinal
     ): SortedMap[Address, List[SpendAction]] =
-      spendActionsList
-        .reduceOption(_ ++ _)
-        .getOrElse(SortedMap.empty)
+      if (lastUnsyncGlobalSnapshotOrdinal > updatedLastSyncGlobalFromPeersInConsensus) {
+        spendActionsList
+          .reduceOption(_ |+| _)
+          .getOrElse(SortedMap.empty)
+      } else {
+        spendActionsList
+          .reduceOption(_ ++ _)
+          .getOrElse(SortedMap.empty)
+      }
 
     private def acceptTransactionRefs(
       lastTxRefs: SortedMap[Address, TransactionReference],
