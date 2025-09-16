f8b97acb3f4ff9164c0c8586ecb0320b7f0179c1
Marcus Vinicius Girolneto Sousa <marcus.girolneto@gmail.com>
2025-09-16 14:42:22 -0300

    fix: fixing last N global snapshot storage (#1320)
    I've inspected the memory dump and then I've figured out the following
    behavior:
    <img width="2560" height="1070" alt="Screenshot 2025-09-16 at 12 45 04"
    src="https://github.com/user-attachments/assets/1481ead1-1a84-40af-ae45-ff64d87a08b6"
    />
    
    The storage was consuming the entire memory, and then I found a bug
    where we're storing all states for all snapshots without limiting. We
    definitely don't need this. I also reduced the lastN
    
    (cherry picked from commit 86ce43eb3ca457f6d0baf25b59479a79c04a92dd)

--- modules/node-shared/src/main/scala/io/constellationnetwork/node/shared/infrastructure/snapshot/storage/LastNGlobalSnapshotStorage.scala
+++ modules/node-shared/src/main/scala/io/constellationnetwork/node/shared/infrastructure/snapshot/storage/LastNGlobalSnapshotStorage.scala
@@ -11,6 +11,7 @@ import io.constellationnetwork.node.shared.domain.collateral.LatestBalances
 import io.constellationnetwork.node.shared.domain.snapshot.Validator.isNextSnapshot
 import io.constellationnetwork.node.shared.domain.snapshot.services.GlobalL0Service
 import io.constellationnetwork.node.shared.domain.snapshot.storage.{LastNGlobalSnapshotStorage, SnapshotStorage}
+import io.constellationnetwork.node.shared.infrastructure.snapshot.storage.LastSnapshotStorage.make
 import io.constellationnetwork.schema.address.Address
 import io.constellationnetwork.schema.balance.Balance
 import io.constellationnetwork.schema.height.Height
