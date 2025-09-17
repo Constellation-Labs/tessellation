25a1881ff2d488fa857ef403c1a383b68b30f527
effects-ai <effects-ai@users.noreply.github.com>
2025-09-16 21:58:12 -0700

    fix: delegate reward info calculation now works with non-epoch snapshots

--- modules/dag-l0/src/test/scala/io/constellationnetwork/dag/l0/infrastructure/snapshot/GlobalSnapshotConsensusFunctionsSuite.scala
+++ modules/dag-l0/src/test/scala/io/constellationnetwork/dag/l0/infrastructure/snapshot/GlobalSnapshotConsensusFunctionsSuite.scala
@@ -18,6 +18,7 @@ import io.constellationnetwork.dag.l0.domain.snapshot.programs.{
   SnapshotBinaryFeeCalculator,
   UpdateNodeParametersCutter
 }
+import io.constellationnetwork.dag.l0.infrastructure.rewards.RewardsService
 import io.constellationnetwork.dag.l0.infrastructure.snapshot.event.{GlobalSnapshotEvent, StateChannelEvent}
 import io.constellationnetwork.env.AppEnvironment
 import io.constellationnetwork.env.AppEnvironment.Dev
@@ -47,6 +48,7 @@ import io.constellationnetwork.node.shared.domain.swap.block._
 import io.constellationnetwork.node.shared.domain.tokenlock.block._
 import io.constellationnetwork.node.shared.infrastructure.consensus.trigger
 import io.constellationnetwork.node.shared.infrastructure.consensus.trigger.{ConsensusTrigger, EventTrigger}
+import io.constellationnetwork.node.shared.infrastructure.delegatedStake.{RewardsInfoCalculator, RewardsInfoStorage}
 import io.constellationnetwork.node.shared.infrastructure.metrics.Metrics
 import io.constellationnetwork.node.shared.infrastructure.snapshot._
 import io.constellationnetwork.node.shared.nodeSharedKryoRegistrar
@@ -313,7 +315,7 @@ object GlobalSnapshotConsensusFunctionsSuite extends MutableIOSuite with Checker
     sp: SecurityProvider[IO],
     h: Hasher[IO],
     m: Metrics[IO]
-  ): GlobalSnapshotConsensusFunctions[IO] = {
+  ): IO[GlobalSnapshotConsensusFunctions[IO]] = {
     implicit val hs = HasherSelector.forSyncAlwaysCurrent(h)
 
     val spendActionValidator = SpendActionValidator.make[IO]
@@ -349,18 +351,23 @@ object GlobalSnapshotConsensusFunctionsSuite extends MutableIOSuite with Checker
       ): IO[NonNegLong] =
         event.value.snapshotBinary.value.fee.value.pure[IO]
     }
-    GlobalSnapshotConsensusFunctions
-      .make[IO](
-        snapshotAcceptanceManager,
-        collateral,
-        classicRewards,
-        delegatorRewards,
-        GlobalSnapshotEventCutter.make[IO](20_000_000, feeCalculator),
-        UpdateNodeParametersCutter.make(100),
-        AppEnvironment.Dev,
-        delegatedRewardsConfigProvider,
-        SnapshotOrdinal.MinValue
-      )
+
+    RewardsInfoStorage.make.map { rewardsInfoStorage =>
+      val rewardsInfoCalculator = RewardsInfoCalculator.make(delegatorRewards)
+      val rewardsService = RewardsService[IO](classicRewards, delegatorRewards, rewardsInfoCalculator, rewardsInfoStorage)
+
+      GlobalSnapshotConsensusFunctions
+        .make[IO](
+          snapshotAcceptanceManager,
+          collateral,
+          rewardsService,
+          GlobalSnapshotEventCutter.make[IO](20_000_000, feeCalculator),
+          UpdateNodeParametersCutter.make(100),
+          AppEnvironment.Dev,
+          delegatedRewardsConfigProvider,
+          SnapshotOrdinal.MinValue
+        )
+    }
   }
 
   def getTestData(
@@ -373,7 +380,7 @@ object GlobalSnapshotConsensusFunctionsSuite extends MutableIOSuite with Checker
     for {
       keyPair <- KeyPairGenerator.makeKeyPair[F]
 
-      gscf = mkGlobalSnapshotConsensusFunctions
+      gscf <- mkGlobalSnapshotConsensusFunctions
       facilitators = Set.empty[PeerId]
 
       genesis = GlobalSnapshot.mkGenesis(Map.empty, EpochProgress.MinValue)
