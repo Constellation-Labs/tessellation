9f73dd1c4e8d0c6fe54640a9f3324cb8a40afff3
Marcus Vinicius Girolneto Sousa <marcus.girolneto@gmail.com>
2025-09-22 11:22:02 -0300

    chore: adding logs

--- modules/node-shared/src/main/scala/io/constellationnetwork/node/shared/infrastructure/snapshot/DataApplicationSnapshotAcceptanceManager.scala
+++ modules/node-shared/src/main/scala/io/constellationnetwork/node/shared/infrastructure/snapshot/DataApplicationSnapshotAcceptanceManager.scala
@@ -171,58 +171,59 @@ object DataApplicationSnapshotAcceptanceManager {
               (newState, oldFeeTxns, oldAcceptedBlocks, oldRejectedBlocks)
             }
           } else {
-            blocksToProcess.foldLeftM(initialResult) {
-              case ((currentState, accFeeTransactions, accAcceptedBlocks, accNotAcceptedBlocks), dataBlock) =>
-                val dataTransactions = dataBlock.value.dataTransactions
+            logger.info(s"Starting to process blocks: ${blocksToProcess.map(_.roundId)}") >>
+              blocksToProcess.foldLeftM(initialResult) {
+                case ((currentState, accFeeTransactions, accAcceptedBlocks, accNotAcceptedBlocks), dataBlock) =>
+                  val dataTransactions = dataBlock.value.dataTransactions
 
-                val dataTransactionsValidations =
-                  dataTransactions.traverse(validateDataTransactionsL0(_, service, balances, currentOrdinal, dataState)).map(_.reduce)
+                  val dataTransactionsValidations =
+                    dataTransactions.traverse(validateDataTransactionsL0(_, service, balances, currentOrdinal, dataState)).map(_.reduce)
 
-                dataTransactionsValidations.flatTap { validation =>
-                  if (validation.isValid)
-                    logger.debug(s"Validating block with roundId=${dataBlock.value.roundId}")
-                  else
-                    logger.debug(s"Block ${dataBlock.value.roundId} is invalid: ${validation.fold(_.toList.mkString(", "), _ => "")}")
-                }.flatMap {
-                  case Valid(_) =>
-                    val dataTransactionsAsList = dataTransactions.toList
-                    val dataUpdates = getDataUpdates(dataTransactionsAsList)
-                    val feeTransactions = getFeeTransactions(dataTransactionsAsList)
+                  dataTransactionsValidations.flatTap { validation =>
+                    if (validation.isValid)
+                      logger.info(s"Validating block with roundId=${dataBlock.value.roundId}")
+                    else
+                      logger.info(s"Block ${dataBlock.value.roundId} is invalid: ${validation.fold(_.toList.mkString(", "), _ => "")}")
+                  }.flatMap {
+                    case Valid(_) =>
+                      val dataTransactionsAsList = dataTransactions.toList
+                      val dataUpdates = getDataUpdates(dataTransactionsAsList)
+                      val feeTransactions = getFeeTransactions(dataTransactionsAsList)
 
-                    for {
-                      _ <- logger.debug(s"Block ${dataBlock.value.roundId} is valid")
-                      result <- service.combine(currentState, dataUpdates).map { newState =>
+                      for {
+                        _ <- logger.info(s"Block ${dataBlock.value.roundId} is valid")
+                        result <- service.combine(currentState, dataUpdates).map { newState =>
+                          (
+                            newState,
+                            accFeeTransactions ++ feeTransactions,
+                            accAcceptedBlocks :+ dataBlock,
+                            accNotAcceptedBlocks
+                          )
+                        }
+                        _ <- logger.info(s"SharedArtifacts produced: ${result._1.sharedArtifacts}")
+                      } yield result
+
+                    case Invalid(err) =>
+                      Async[F].pure(
                         (
-                          newState,
-                          accFeeTransactions ++ feeTransactions,
-                          accAcceptedBlocks :+ dataBlock,
-                          accNotAcceptedBlocks
+                          currentState,
+                          accFeeTransactions,
+                          accAcceptedBlocks,
+                          accNotAcceptedBlocks :+ (dataBlock, DataBlockNotAccepted(err.toString))
                         )
-                      }
-                      _ <- logger.debug(s"SharedArtifacts produced: ${result._1.sharedArtifacts}")
-                    } yield result
-
-                  case Invalid(err) =>
-                    Async[F].pure(
-                      (
-                        currentState,
-                        accFeeTransactions,
-                        accAcceptedBlocks,
-                        accNotAcceptedBlocks :+ (dataBlock, DataBlockNotAccepted(err.toString))
                       )
-                    )
-                }.handleErrorWith { err =>
-                  logger.error(err)(s"Exception during block validation for roundId=${dataBlock.value.roundId}") >>
-                    Async[F].pure(
-                      (
-                        currentState,
-                        accFeeTransactions,
-                        accAcceptedBlocks,
-                        accNotAcceptedBlocks :+ (dataBlock, DataBlockNotAccepted(err.getMessage))
+                  }.handleErrorWith { err =>
+                    logger.error(err)(s"Exception during block validation for roundId=${dataBlock.value.roundId}") >>
+                      Async[F].pure(
+                        (
+                          currentState,
+                          accFeeTransactions,
+                          accAcceptedBlocks,
+                          accNotAcceptedBlocks :+ (dataBlock, DataBlockNotAccepted(err.getMessage))
+                        )
                       )
-                    )
-                }
-            }
+                  }
+              }
           }
         }
 
