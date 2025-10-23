b4ce920747d408d55d8e3e067e5b53e25518ef2d
Marcus Vinicius Girolneto Sousa <marcus.girolneto@gmail.com>
2025-10-23 11:57:56 -0300

    fix: fixing GL0 storage initialization

--- modules/dag-l0/src/main/scala/io/constellationnetwork/dag/l0/infrastructure/snapshot/GlobalSnapshotTraverse.scala
+++ modules/dag-l0/src/main/scala/io/constellationnetwork/dag/l0/infrastructure/snapshot/GlobalSnapshotTraverse.scala
@@ -2,7 +2,6 @@ package io.constellationnetwork.dag.l0.infrastructure.snapshot
 
 import cats.Parallel
 import cats.data.NonEmptyChain
-import cats.effect.IO
 import cats.effect.kernel.Async
 import cats.syntax.all._
 
@@ -142,10 +141,18 @@ object GlobalSnapshotTraverse {
                       .map(_ -> inc)
                   } yield (hashed, context)
                 }
-
-                _ <- lastNGlobalSnapshotStorage.set(hashedInc, updatedState)
-                _ <- lastGlobalSnapshotStorage.set(hashedInc, updatedState)
-                _ <- HasherSelector[F].forOrdinal(inc.ordinal)(implicit hasher => globalSnapshotStorage.prepend(inc, updatedState))
+                _ <-
+                  if (hashedInc.ordinal > hashedFirstInc.ordinal) {
+                    lastNGlobalSnapshotStorage.set(hashedInc, updatedState)
+                  } else ().pure
+                _ <-
+                  if (hashedInc.ordinal > hashedFirstInc.ordinal) {
+                    lastGlobalSnapshotStorage.set(hashedInc, updatedState)
+                  } else ().pure
+                _ <-
+                  if (hashedInc.ordinal > hashedFirstInc.ordinal) {
+                    HasherSelector[F].forOrdinal(inc.ordinal)(implicit hasher => globalSnapshotStorage.prepend(inc, updatedState))
+                  } else ().pure
               } yield (updatedState, inc)
           }
         } yield (info, lastInc)
