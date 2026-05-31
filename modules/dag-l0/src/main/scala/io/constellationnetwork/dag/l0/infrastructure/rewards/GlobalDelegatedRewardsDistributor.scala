5996cbf22e1f6bcc4af519cfee598563c4c9f265
effects-ai <effects-ai@users.noreply.github.com>
2026-05-31 13:39:20 -0700

    fix: use replaced token locks for rewards calculation

--- modules/dag-l0/src/main/scala/io/constellationnetwork/dag/l0/infrastructure/rewards/GlobalDelegatedRewardsDistributor.scala
+++ modules/dag-l0/src/main/scala/io/constellationnetwork/dag/l0/infrastructure/rewards/GlobalDelegatedRewardsDistributor.scala
@@ -258,7 +258,7 @@ object GlobalDelegatedRewardsDistributor {
     }
 
     private def getStakedAmount(stakeRecord: DelegatedStakeRecord): Long =
-      stakeRecord.event.value.amount.value.value + stakeRecord.rewards.value
+      stakeRecord.amount.value.value + stakeRecord.rewards.value
 
     private def getTotalActiveStake(
       activeDelegatedStakes: SortedMap[Address, SortedSet[DelegatedStakeRecord]]
